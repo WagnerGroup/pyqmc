4ccdbb36819ad66b6994cd1e2f0349fd73fa674a
Will Wheeler <willwheelera@gmail.com>
2019-05-08 08:49:09 -0500

    Add tests.py: has tests for gradient() and laplacian() in wave function objects

---
+++ tests.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+def test_wf_gradient(wf, epos, delta=1e-5):
+    """ 
+    Parameters:
+        wf: a wavefunction object with functions wf.recompute(epos), wf.testvalue(e,epos) and wf.gradient(e,epos)
+        epos: nconf x nelec x 3 position array to set the wf object
+        delta: the finite difference step; 1e-5 to 1e-6 seem to be the best compromise between accuracy and machine precision
+    Tests wf.gradient(e,epos) against numerical derivatives of wf.testvalue(e,epos)
+    For gradient and testvalue:
+        e is the electron index
+        epos is nconf x 3 positions of electron e
+    wf.testvalue(e,epos) should return a ratio: the wf value at the position where electron e is moved to epos divided by the original value
+    wf.gradient(e,epos) should return a ratio: the wf gradient of moving electron e from position epos divided by the original value of the wave function (which may have a different position of e)
+    """
+    print('delta={0}'.format(delta))
+    nconf, nelec = epos.shape[0:2]
+    wf.recompute(epos)
+    maxerror=0
+    grad = np.zeros(epos.shape)
+    numeric = np.zeros(epos.shape)
+    for e in range(nelec):
+        grad[:,e,:] = wf.gradient(e, epos[:,e,:]).T
+        for d in range(0,3):
+            eposnew=epos.copy()
+            eposnew[:,e,d]+=delta
+            plusval=wf.testvalue(e,eposnew[:,e,:])
+            eposnew[:,e,d]-=2*delta
+            minuval=wf.testvalue(e,eposnew[:,e,:])
+            numeric[:,e,d] = (plusval - minuval)/(2*delta)
+    maxerror = np.amax(np.abs(grad-numeric))
+    normerror = np.linalg.norm(grad-numeric)
+    print('maxerror', maxerror, np.log10(maxerror))
+    print('normerror', normerror, np.log10(normerror))
+
+def test_wf_laplacian(wf, epos, delta=1e-5):
+    """ 
+    Parameters:
+        wf: a wavefunction object with functions wf.recompute(epos), wf.gradient(e,epos) and wf.laplacian(e,epos)
+        epos: nconf x nelec x 3 position array to set the wf object
+        delta: the finite difference step; 1e-5 to 1e-6 seem to be the best compromise between accuracy and machine precision
+    Tests wf.laplacian(e,epos) against numerical derivatives of wf.gradient(e,epos)
+    For gradient and laplacian:
+        e is the electron index
+        epos is nconf x 3 positions of electron e
+    wf.gradient(e,epos) should return a ratio: the wf gradient of moving electron e from position epos divided by the original value of the wave function (which may have a different position of e)
+    wf.laplacian(e,epos) should return a ratio: the wf laplacian at the position where electron e is moved to epos divided by the original value
+    """
+    print('delta={0}'.format(delta))
+    nconf, nelec = epos.shape[0:2]
+    wf.recompute(epos)
+    maxerror=0
+    lap = np.zeros(epos.shape[:2])
+    numeric = np.zeros(epos.shape[:2])
+    for e in range(nelec):
+        print('shape of lap', wf.laplacian(e,epos[:,e,:]).shape); quit()
+        lap[:,e] = wf.laplacian(e, epos[:,e,:])
+        for d in range(0,3):
+            eposnew=epos.copy()
+            eposnew[:,e,d]+=delta
+            plusval=wf.gradient(e,eposnew[:,e,:])[d]
+            eposnew[:,e,d]-=2*delta
+            minuval=wf.gradient(e,eposnew[:,e,:])[d]
+            numeric[:,e] += (plusval - minuval)/(2*delta)
+    maxerror = np.amax(np.abs(lap-numeric))
+    normerror = np.linalg.norm(lap-numeric)
+    print('maxerror', maxerror, np.log10(maxerror))
+    print('normerror', normerror, np.log10(normerror))
+
+
+if __name__=='__main__':
+    from pyscf import lib, gto, scf
+    from slater import PySCFSlaterRHF
+    from jastrow import Jastrow2B
+    mol = gto.M(atom='Li 0. 0. 0.; H 0. 0. 1.5', basis='cc-pvtz',unit='bohr')
+    mf = scf.RHF(mol).run()
+    wf=PySCFSlaterRHF(10,mol,mf)
+    #wf=Jastrow2B(10,mol)
+    for i in range(5):
+        epos=np.random.randn(10,4,3)
+        test_wf_gradient(wf, epos, delta=1e-5)
+    for i in range(5):
+        epos=np.random.randn(10,4,3)
+        test_wf_laplacian(wf, epos, delta=1e-5)

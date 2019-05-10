ae07a6ee3177fe0879784f1db750277385514e1c
Will Wheeler <willwheelera@gmail.com>
2019-05-10 14:30:15 -0500

    mc.py: add initial_guess_vectorized(), assigns leftover electrons WITH replacement, about 200x faster for nconfig=5000

--- mc.py
+++ mc.py
@@ -31,7 +31,30 @@ def initial_guess(mol,nconfig,r=1.0):
                     count+=1
     return epos
     
+def initial_guess_vectorize(mol,nconfig,r=1.0):
+    """ Generate an initial guess by distributing electrons near atoms
+    proportional to their charge."""
+    nelec=np.sum(mol.nelec)
+    epos=np.zeros((nconfig,nelec,3))
+    wts=mol.atom_charges()
+    wts=wts/np.sum(wts)
 
+    # assign electrons to atoms based on atom charges
+    # assign the minimum number first, and assign the leftover ones randomly
+    # this algorithm chooses atoms *with replacement* to assign leftover electrons
+
+    for s in [0,1]:
+        neach=np.array(np.floor(mol.nelec[s]*wts),dtype=int) # integer number of elec on each atom
+        nleft=mol.nelec[s]*wts-neach # fraction of electron unassigned on each atom
+        nassigned=np.sum(neach) # number of electrons assigned
+        totleft=int(mol.nelec[s]-nassigned) # number of electrons not yet assigned
+        bins=np.cumsum(nleft)/totleft
+        inds = np.digitize(np.random.random((nconfig,totleft)), bins)
+        ind0=s*mol.nelec[0]
+        epos[:,ind0:ind0+nassigned,:] = np.repeat(mol.atom_coords(),neach,axis=0)[np.newaxis] # assign core electrons
+        epos[:,ind0+nassigned:ind0+mol.nelec[s],:] = mol.atom_coords()[inds] # assign remaining electrons
+    epos+=r*np.random.randn(*epos.shape) # random shifts from atom positions
+    return epos
 
 def vmc(mol,wf,coords,nsteps=10000,tstep=0.5,accumulators=None):
     if accumulators is None:
@@ -78,6 +101,19 @@ def test():
     print('mean field',mf.energy_tot(),'vmc estimation', np.mean(df['energytotal'][warmup:]),np.std(df['energytotal'][warmup:]))
     print('dipole',np.mean(np.asarray(df['dipolevec'][warmup:]),axis=0))
     
+def test_init_guess_timing():
+    import time
+    mol = gto.M(atom='Li 0. 0. 0.; Li 0. 0. 1.5', basis='cc-pvtz',unit='bohr',verbose=5)
+    mf = scf.RHF(mol).run()
+    nconf=5000
+    wf=PySCFSlaterRHF(nconf,mol,mf)
+    for j in range(5):
+        for i,func in enumerate([initial_guess, initial_guess_vectorize]):
+            start = time.time()
+            coords = func(mol,nconf) 
+            print(time.time()-start)
+    
+
 if __name__=="__main__":
     import cProfile, pstats, io
     from pstats import SortKey

0a68080af89b2e7a50e535114e4546f953bf1d07
Lucas K. Wagner <lucas.wagner@gmail.com>
2019-05-13 14:33:57 -0500

    fix updateinternals() for multiplywf

--- multiplywf.py
+++ multiplywf.py
@@ -20,9 +20,9 @@ class MultiplyWF:
         v2=self.wf2.recompute(epos)
         return v1[0]*v2[0],v1[1]+v2[1]
 
-    def updateinternals(self,epos,mask=None):
-        self.wf1.updateinternals(epos,mask)
-        self.wf2.updateinternals(epos,mask)
+    def updateinternals(self,e,epos,mask=None):
+        self.wf1.updateinternals(e,epos,mask)
+        self.wf2.updateinternals(e,epos,mask)
 
 
     def value(self):

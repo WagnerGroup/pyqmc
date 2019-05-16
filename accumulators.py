47c7b99201e1defb4a3bb716e41e7d232b7ab764
Munoz <armunoz3@illinois.edu>
2019-05-16 09:53:47 -0500

    Remove configs from wf.pgradient for now

--- accumulators.py
+++ accumulators.py
@@ -16,7 +16,7 @@ class PGradAccumulator:
   def __call__(self,configs,wf):
     d=self.EnergyAccumulator(configs,wf)
     energy = d['total']
-    pgrad = wf.pgradient(configs)
+    pgrad = wf.pgradient()
     for k,grad in pgrad.items():
         d['dpH_'+k] = energy[:,np.newaxis,np.newaxis]*grad
         d['dppsi_'+k] = grad

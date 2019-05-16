Fixed jastrow derivatives and testing for parameter derivatives.

testwf.py
@@ -81,18 +81,17 @@ def test_wf_pgradient(wf,configs,delta=1e-5):
     #and self.wf2.parameters, resetting the reference breaks it.)
     #
     for k in gradient.keys(): #We only check the gradients that are exposed.
-        nparms=np.prod(wf.parameters[k].shape)
-        indices=np.unravel_index(range(nparms),wf.parameters[k].shape)
-
+        flt=wf.parameters[k].ravel()
+        nparms=len(flt)
         numgrad=np.zeros((configs.shape[0],nparms))
-        for i,ind in enumerate(indices):
-            wf.parameters[k][ind]+=delta
+        for i,c in enumerate(flt):
+            flt[i]+=delta
             plusval=wf.recompute(configs)
-            wf.parameters[k][ind]-=2*delta
+            flt[i]-=2*delta
             minuval=wf.recompute(configs)
             numgrad[:,i] = (plusval[0]*baseval[0]*np.exp(plusval[1]-baseval[1]) 
                     - minuval[0]*baseval[0]*np.exp(minuval[1]-baseval[1]))/(2*delta)
-            wf.parameters[k][ind]+=delta
+            flt[i]+=delta
         #print(gradient[k],numgrad)            
         error[k]=(np.amax(np.abs(gradient[k]-numgrad)),
                   np.mean(np.abs(gradient[k]-numgrad)))
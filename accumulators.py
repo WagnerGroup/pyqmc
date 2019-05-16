Fix broadcasting bug in PGradAccumulator

accumulators.py
@@ -1,25 +1,25 @@
 import numpy as np 
 from energy import * 
-
+"""returns energy of each configuration in a dictionary"""
 class EnergyAccumulator:
   def __init__(self, mol):
     self.mol = mol
 
-  def __call__(configs, wf):
-    return energy(mol, configs, wf)
+  def __call__(self,configs, wf):
+    return energy(self.mol, configs, wf)
 
 
-
+"""returns parameter derivatives of energy for each configuration"""
 class PGradAccumulator:
-  def __init__ (self,EnergyAccumalator):
+  def __init__ (self,EnergyAccumulator):
     self.EnergyAccumulator = EnergyAccumulator
 
-  def __call__(configs,wf):
-    d=EnergyAccumulator(configs,wf)
+  def __call__(self,configs,wf):
+    d=self.EnergyAccumulator(configs,wf)
     energy = d['total']
-    pgrad = wf.pgradient()
+    pgrad = wf.pgradient(configs)
     for k,grad in pgrad.items():
-        d['dpH_'+k] = energy*grad
+        d['dpH_'+k] = energy[:,np.newaxis,np.newaxis]*grad
         d['dppsi_'+k] = grad
     return d
 
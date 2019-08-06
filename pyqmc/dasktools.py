fixed linemin to average over all weights, at least for dask. parsl will not work right now

pyqmc/dasktools.py
@@ -91,9 +91,20 @@ def dist_lm_sampler(wf, configs, params, pgrad_acc, npartitions=None, client=Non
 
     stepresults = []
     for r in allruns:
-        stepresults.extend(r.result())
+        stepresults.append(r.result())
 
-    return stepresults
+    keys=stepresults[0][0].keys()
+    #This will be a list of dictionaries
+    final_results=[]
+    for p in range(len(params)):
+        df={}
+        for k in keys:
+            #print(k,flush=True)
+            #print(stepresults[0][p][k])
+            df[k]=np.concatenate([x[p][k] for x in stepresults],axis=0)
+        final_results.append(df)
+
+    return final_results
 
 
 def line_minimization(*args, client, **kwargs):

pyqmc/linemin.py
@@ -6,7 +6,7 @@ import scipy
 def sr_update(pgrad, Sij, step, eps=0.1):
     invSij = np.linalg.inv(Sij + eps * np.eye(Sij.shape[0]))
     v = np.einsum("ij,j->i", invSij, pgrad)
-    return -v * step / np.linalg.norm(v)
+    return -v * step #/ np.linalg.norm(v)
 
 
 def sd_update(pgrad, Sij, step, eps=0.1):
@@ -146,7 +146,7 @@ def line_minimization(
         stepsdata = lm(wf, coords, params, pgrad_acc, **lmoptions)
         dfs = []
         for data, p, step in zip(stepsdata, params, steps):
-            en = np.mean(data["total"])
+            en = np.mean(data["total"]*data['weight'])/np.mean(data['weight'])
             dfs.append(
                 {
                     "en": en,
@@ -224,11 +224,14 @@ def lm_sampler(wf, configs, params, pgrad_acc):
             wf.parameters[k] = newparms[k]
         psi = wf.recompute(configs)[1]  # recompute gives logdet
         rawweights = np.exp(2 * (psi - psi0))  # convert from log(|psi|) to |psi|**2
-        weights = rawweights / np.mean(rawweights)
+        #weights = rawweights / np.mean(rawweights)
         df = pgrad_acc(configs, wf)
-        for k in df:
-            df[k] = np.einsum(
-                "i,i...->i...", weights, df[k]
-            )  # reweight all averaged quantities by sampling probability
+        df['weight']=rawweights
+        
+
+#        for k in df:
+#            df[k] = np.einsum(
+#                "i,i...->i...", weights, df[k]
+#            )  # reweight all averaged quantities by sampling probability
         data.append(df)
     return data
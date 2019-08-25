dc2756bc7aa059c02ba53459561a7a9cb1f306f3
Shivesh Pathak <sapatha2@illinois.edu>
2019-08-25 17:54:05 -0500

    added mask constraints to LinearTransofmr

--- pyqmc/__init__.py
+++ pyqmc/__init__.py
@@ -30,7 +30,29 @@ def slater_jastrow(mol, mf, abasis=None, bbasis=None):
     return wf
 
 
-def gradient_generator(mol, wf, to_opt=None):
+def gradient_generator(mol, wf, to_opt=None, freeze=None):
     return PGradTransform(
-        EnergyAccumulator(mol), LinearTransform(wf.parameters, to_opt)
+        EnergyAccumulator(mol), LinearTransform(wf.parameters, to_opt, freeze)
     )
+
+def default_constraints(wf,det_pivot=0):
+    import numpy as np 
+    freeze = {}
+    if('det_coeff' in wf.parameters.keys()): 
+        freeze['det_coeff'] = np.zeros(wf.parameters['det_coeff'].shape).astype(bool)
+        freeze['det_coeff'][det_pivot] = True #Determinant coefficient pivot 
+
+    return freeze 
+
+from pyscf import lib, gto, scf, mcscf
+import pyqmc.testwf as testwf
+from pyqmc.mc import vmc, initial_guess
+from pyqmc.accumulators import EnergyAccumulator
+from pyqmc.multislater import MultiSlater
+mol = gto.M(atom="Li 0. 0. 0.; H 0. 0. 1.5", basis="cc-pvtz", unit="bohr", spin=0)
+mf = scf.RHF(mol).run()
+mc = mcscf.CASCI(mf,ncas=2,nelecas=(1,1))
+mc.kernel()
+wf = MultiSlater(mol, mf, mc)
+f = default_constraints(wf)
+print(f)
--- pyqmc/accumulators.py
+++ pyqmc/accumulators.py
@@ -20,11 +20,24 @@ class EnergyAccumulator:
 
 
 class LinearTransform:
-    def __init__(self, parameters, to_opt=None):
+    """
+    Linearize a dictionary of wf parameters, only to_opt 
+    terms optimized. A dict freeze can be used to freeze
+    certain wf parameters. 
+    """
+    def __init__(self, parameters, to_opt=None, freeze=None):
         if to_opt is None:
             self.to_opt = list(parameters.keys())
         else:
             self.to_opt = to_opt
+
+        if freeze is None:
+            freeze = {}
+            for i, k in enumerate(parameters):
+                freeze[k] = np.zeros(parameters[k].shape).astype(bool)
+            self.frozen = freeze       
+        else: 
+            self.frozen = freeze
 
         self.shapes = np.array([parameters[k].shape for k in self.to_opt])
         self.slices = np.array([np.prod(s) for s in self.shapes])
@@ -40,11 +53,14 @@ class LinearTransform:
 
     def serialize_gradients(self, pgrad):
         """Convert the dictionary to a linear list
-        of gradients
+        of gradients, mask allows for certain fixed parameters
         """
         grads = []
         for k in self.to_opt:
-            grads.append(pgrad[k].reshape((pgrad[k].shape[0], -1)))
+            mask_grads = np.ma.array(pgrad[k],\
+                mask = np.repeat(self.frozen[k],pgrad[k].shape[0],axis=0),
+                fill_value = 0).reshape(pgrad[k].shape[0],-1)
+            grads.append(mask_grads.filled())
         return np.concatenate(grads, axis=1)
 
     def deserialize(self, parameters):

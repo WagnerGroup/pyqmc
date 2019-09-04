8a80217fe046542a2403655481c4642d87d09a55
Will Wheeler <willwheelera@gmail.com>
2019-09-04 18:00:13 -0500

    test_multislater.py: add test for multi-slater-jastrow

--- tests/test_multislater.py
+++ tests/test_multislater.py
@@ -12,6 +12,7 @@ from pyqmc.mc import vmc, initial_guess
 from pyqmc.accumulators import EnergyAccumulator
 from pyqmc.multislater import MultiSlater
 from pyqmc.coord import OpenConfigs
+import pyqmc
 
 def test():
     """ 
@@ -25,6 +26,22 @@ def test():
         mc = mcscf.CASCI(mf,ncas=4,nelecas=(1,1))
         mc.kernel()
         wf = MultiSlater(mol, mf, mc)
+
+        epsilon = 1e-5
+        nconf = 10
+        nelec = np.sum(mol.nelec)
+        epos = OpenConfigs(np.random.randn(nconf, nelec, 3))
+      
+        for k, item in testwf.test_updateinternals(wf, epos).items():
+            assert item < epsilon
+        assert testwf.test_wf_gradient(wf, epos, delta=1e-5)[0] < epsilon
+        assert testwf.test_wf_laplacian(wf, epos, delta=1e-5)[0] < epsilon
+        assert testwf.test_wf_pgradient(wf, epos, delta=1e-5)[0] < epsilon
+
+        #Test same number of elecs
+        mc = mcscf.CASCI(mf,ncas=4,nelecas=(1,1))
+        mc.kernel()
+        wf = pyqmc.default_msj(mol, mf, mc)[0]
 
         epsilon = 1e-5
         nconf = 10

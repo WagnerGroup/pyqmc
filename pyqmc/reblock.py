5bbf7181e76b1eef7ef65074529423a96d7ded08
Will Wheeler <willwheelera@gmail.com>
2019-09-12 23:52:03 -0500

    rewrite optimally_reblock to match previous format

--- pyqmc/reblock.py
+++ pyqmc/reblock.py
@@ -8,8 +8,15 @@ def optimally_reblocked(data):
         of reblocked data.
     """
     opt = opt_block(data)
-    reblocked_data = reblock(data, np.amax(opt))
-    return reblocked_data
+    n_reblock = int(np.amax(opt))
+    rb_data = reblock(data, n_reblock)
+    serr = rb_data.sem(axis=0)
+    d = {"mean": rb_data.mean(axis=0), 
+         "standard error": serr,
+         "standard error error": serr / np.sqrt(2 * (len(rb_data) - 1)),
+         "reblocks": n_reblock,
+    }
+    return pd.DataFrame(d)
 
 def optimally_reblocked_pyblock(data):  # , cols):
     import pyblock
@@ -24,8 +31,11 @@ def optimally_reblocked_pyblock(data):  # , cols):
     reblocked_data["reblocks"] = reblocks
     return reblocked_data
 
-def reblock(df, n, c): 
-    start = time.time()
+def reblock(df, n, c=None): 
+    """
+        Reblocks data according to “Error estimates on averages of correlated data”,
+        H. Flyvbjerg, H.G. Petersen, J. Chem. Phys. 91, 461 (1989).
+    """
     newdf = df.copy()
     if c is not None:
         newdf = newdf[c]
@@ -42,14 +52,11 @@ def opt_block(df):
     reblock each column over samples to find the best block size
     Returns optimal_block, a 1D array with the optimal size for each column in df
     """
-    start = time.time()
     newdf = df.copy()
     iblock = 0
     ndata, nvariables = tuple(df.shape[:2])
     optimal_block = np.array([float('NaN')]*nvariables)
     serr0 = df.sem(axis=0).values
-    print('serr0.shape',serr0.shape, time.time()-start)
-    print('ndata',ndata, time.time()-start)
     statslist = []
     while(newdf.shape[0]>1):
         serr = newdf.sem(axis=0).values
@@ -57,7 +64,6 @@ def opt_block(df):
         statslist.append((iblock, serr.copy()))
 
         n = newdf.shape[0]
-        print(iblock, n, time.time()-start)
         lasteven = n - int(n%2==1)
         newdf = (newdf[:lasteven:2]+newdf[1::2].values)/2
         iblock += 1
@@ -65,9 +71,8 @@ def opt_block(df):
         B3 = 2**(3*iblock)
         inds = np.where(B3 >= 2*ndata*(serr/serr0)**4)[0]
         optimal_block[inds] = iblock
-        print(iblock, len(inds), time.time()-start)
 
-  return optimal_block
+    return optimal_block
 
 def test_reblocking():
     """
@@ -81,20 +86,6 @@ def test_reblocking():
             https://pyblock.readthedocs.io/en/latest/tutorial.html.
         """
         return np.convolve(np.random.randn(2 ** N), np.ones(2 ** L) / 10, "same")
-
-    def reblock(data, reblocks, col):
-        """
-            Reblocks data according to “Error estimates on averages of correlated data”,
-            H. Flyvbjerg, H.G. Petersen, J. Chem. Phys. 91, 461 (1989).
-        """
-        edat = data[col].values
-        n = len(edat)
-        for i in range(reblocks):
-            edat_prime = []
-            for j in range(1, int(len(edat) / 2 + 1)):
-                edat_prime.append((edat[2 * j - 2] + edat[2 * j - 1]) / 2)
-            edat = edat_prime
-        return np.array(edat)
 
     n = 11
     cols = ["test_data1", "test_data2"]

b5d5646d08bc7d6916da6fd87e7905da61fc4091
Will Wheeler <willwheelera@gmail.com>
2019-11-15 18:04:35 -0600

    create periodic_he.py example for using SlaterPBC

---
+++ examples/perodic_he.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import numpy as np
+from pyscf.pbc import gto, scf
+import pyqmc
+from pyqmc.slaterpbc import PySCFSlaterPBC
+
+
+def read_nk():
+    import sys
+
+    args = sys.argv[1:]
+    if len(args) != 1:
+        print("usage: python driver.py nk-per-direction")
+        return -1
+    else:
+        nk = int(args[0])
+        return nk
+
+
+def run_scf(nk):
+    cell = gto.Cell()
+    cell.atom = """
+    He 0.000000000000   0.000000000000   0.000000000000
+    """
+    cell.basis = "gth-dzvp"
+    cell.pseudo = "gth-pade"
+    cell.a = """
+    5.61, 0.00, 0.00
+    0.00, 5.61, 0.00
+    0.00, 0.00, 5.61"""
+    cell.unit = "B"
+    cell.verbose = 5
+    cell.build()
+
+    kpts = cell.make_kpts([nk, nk, nk])
+    kmf = scf.KRHF(cell, exxdiv=None).density_fit()
+    kmf.kpts = kpts
+    ehf = kmf.kernel()
+
+    print("EHF")
+    print(ehf)
+    return cell, kmf
+
+
+if __name__ == "__main__":
+    import pandas as pd
+
+    nconfigs = 100
+    for nk in [2]:
+        # Run SCF
+        cell, kmf = run_scf(nk)
+
+        # Set up wf and configs
+        supercell = np.eye(3) * nk
+        wf = PySCFSlaterPBC(cell, kmf, supercell=supercell)
+        configs = pyqmc.initial_guess(cell, nconfigs, supercell=supercell)
+
+        # Warm up VMC
+        df, configs = pyqmc.vmc(wf, configs, nsteps=40, verbose=True)
+
+        # Initialize energy accumulator (and Ewald)
+        enacc = pyqmc.EnergyAccumulator(cell, supercell=supercell)
+
+        # Run VMC
+        df, configs = pyqmc.vmc(
+            wf, configs, nsteps=20, accumulators={"energy": enacc}, verbose=True
+        )
+
+        df = pd.DataFrame(df)
+        print(df)
+        df.to_csv("pbc_he_nk{0}.csv".format(nk))

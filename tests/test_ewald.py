8faec1e9518e09a61ec2fbff3e4e641a5b70b6d6
Will Wheeler <willwheelera@gmail.com>
2019-11-20 13:24:00 -0600

    test_ewald.py: add assert statements, fix consistency and clean up

--- tests/test_ewald.py
+++ tests/test_ewald.py
@@ -1,67 +1,82 @@
 import numpy as np
 import pyqmc
 from pyqmc.coord import PeriodicConfigs
-from pyqmc.slaterpbc import get_pyscf_supercell
+from pyqmc.slaterpbc import get_supercell
 from pyscf.pbc import gto, scf
 
 
+def get_ewald_energy(cell, S, configs):
+    supercell = get_supercell(cell, S)
+    ewald = pyqmc.ewald.Ewald(supercell)
+    configs = PeriodicConfigs(configs, supercell.lattice_vectors())
+    ee, ei, ii = ewald.energy(configs)
+    etot = ee + ei + ii
+    print(dict(ee=ee, ei=ei, ii=ii))
+    print("total energy", etot)
+    return etot
+
+
 def test_ewald_NaCl():
+    print("NaCl ewald energy")
     L = 2.0  # this normalizes n.n. separation to 1 for rock salt structure
-    mol = gto.Cell(
+    cell = gto.Cell(
         atom="""H     {0}      {0}      {0}""".format(0.0),
         basis="sto-3g",
         unit="bohr",
-        charge=0,
         spin=1,
     )
-    mol.build(a=(np.ones((3, 3)) - np.eye(3)) * L / 2)
+    cell.spin = 1
+    cell.build(a=(np.ones((3, 3)) - np.eye(3)) * L / 2, spin=1)
+    cell.spin = 1
 
-    # supercell = np.ones((3,3)) - 2 * np.eye(3)
+    # Primitive cell
+    print("primitive cell")
     S = np.eye(3)
-    supercell = get_pyscf_supercell(mol, S)
-    ewald = pyqmc.ewald.Ewald(supercell)
-
-    # configs = np.ones((1, 4, 3))
-    # configs[:, 1:, :] = np.eye(3)
     configs = np.ones((1, 1, 3)) * L / 2
-    configs = PeriodicConfigs(configs, supercell.lattice_vectors())
+    etot = get_ewald_energy(cell, S, configs)
+    assert np.abs(etot + 1.74756) < 1e-3
 
-    print("NaCl ewald energy")
-    ee, ei, ii = ewald.energy(configs)
-    print(dict(ee=ee, ei=ei, ii=ii))
-    print("total energy", ee + ei + ii)
+    # Conventional cell
+    print("conventional cell")
+    S = np.ones((3, 3)) - 2 * np.eye(3)
+    configs = np.ones((1, 4, 3)) * L / 2
+    configs[:, 1:, :] = np.eye(3) * L / 2
+    etot = get_ewald_energy(cell, S, configs)
+    assert np.abs(etot / 4 + 1.74756) < 1e-3
 
 
 def test_ewald_CaF2():
+    print("CaF2 ewald energy")
     L = 4 / np.sqrt(3)  # this normalizes n.n. separation to 1 for fluorite structure
-    mol = gto.Cell(
+    cell = gto.Cell(
         atom="""He     {0}      {0}      {0}""".format(0.0),
         basis="sto-3g",
         unit="bohr",
-        charge=0,
         spin=0,
     )
-    mol.build(a=(np.ones((3, 3)) - np.eye(3)) * L / 2)
+    cell.build(a=(np.ones((3, 3)) - np.eye(3)) * L / 2)
 
-    # supercell = np.ones((3,3)) - 2 * np.eye(3)
+    # Primitive cell
+    print("primitive cell")
     S = np.eye(3)
-    supercell = get_pyscf_supercell(mol, S)
-    ewald = pyqmc.ewald.Ewald(supercell)
-
-    # cube = np.stack(np.meshgrid(*[[0, 1]]*3, indexing="ij"), axis=-1).reshape((-1, 3))
-    # configs = np.reshape((cube + 0.5) * L / 2, (1, 8, 3))
     configs = np.ones((1, 2, 3)) * L / 4
     configs[0, 1, 1] *= -1
-    configs = PeriodicConfigs(configs, supercell.lattice_vectors())
+    etot = get_ewald_energy(cell, S, configs)
+    assert np.abs(etot + 5.03879) < 1e-3
 
-    print("CaF2 ewald energy")
-    ee, ei, ii = ewald.energy(configs)
-    print(dict(ee=ee, ei=ei, ii=ii))
-    print("total energy", ee + ei + ii)
+    # Conventional cell
+    print("conventional cell")
+    S = np.ones((3, 3)) - 2 * np.eye(3)
+    cube = np.stack(np.meshgrid(*[[0, 1]] * 3, indexing="ij"), axis=-1).reshape((-1, 3))
+    configs = np.reshape((cube + 0.5) * L / 2, (1, 8, 3))
+    etot = get_ewald_energy(cell, S, configs)
+    assert np.abs(etot / 4 + 5.03879) < 1e-3
 
 
 r"""
 https://en.wikipedia.org/wiki/Madelung_constant
+https://aip.scitation.org/doi/pdf/10.1063/1.1731810
+https://chem.libretexts.org/Bookshelves/Inorganic_Chemistry/Map%3A_Inorganic_Chemistry_(Housecroft)/06%3A_Structures_and_energetics_of_metallic_and_ionic_solids/6.13%3A_Lattice_Energy_-_Estimates_from_an_Electrostatic_Model/6.13E%3A_Madelung_Constants
 
 For a lattice of ions of charge $z_i$, the Madelung constant M_i is defined for an ion at site i of the lattice by
 
@@ -75,7 +90,7 @@ So the total electrostatic energy will be the sum of the energies $z_i M_i$, div
 
 The test above is for the NaCl lattice, (H for "plus", and electrons at "minus" positions, nearest neighbor distance chosen to be $r_0=1$). ewald.energy returns the energy of the unit cell (not per ion). For NaCl, 
 
-$M_i = \pm 1.748$,
+$M_i = \pm 1.74756$,
 
 so the total energy of a unit cell of four formula units would be
 
@@ -86,7 +101,7 @@ ewald energy
 {'ee': array([-4.58486208]), 'ei': array([2.17946577]), 'ii': -4.5848620786449485}
 total energy [-6.99025839]
 
-For CaF2, Madelung constant (per F) is -2.52, so the energy of the unit cell should be -20.16
+For CaF2, Madelung constant (per F) is -2.51939, (per Ca is -5.03879)so the energy of the unit cell should be -20.16
 """
 
 if __name__ == "__main__":

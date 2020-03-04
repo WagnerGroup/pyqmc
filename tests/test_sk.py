4a186268c8b664e8a1e5d2274fb149ee26992e9c
Will Wheeler <willwheelera@gmail.com>
2020-03-03 18:34:10 -0600

    complete Sk fcc test

--- tests/test_sk.py
+++ tests/test_sk.py
@@ -6,22 +6,24 @@ import numpy as np
 def test_fcc():
     # ???
     a = 1
-    Lvecs = (np.ones((3, 3)) - np.eye(3)) * a / 2
-    run(Lvecs)
+    Lvecs = np.eye(3) * a
+    configs = np.zeros((1, 4, 3))
+    configs[0, 1:, :] = (np.ones((3, 3)) - np.eye(3)) * a / 2
+
+    run(Lvecs, configs)
 
 
-def run(Lvecs):
+def run(Lvecs, configs):
     Gvecs = np.linalg.inv(Lvecs).T * 2 * np.pi
-    kvecs = np.stack([x.ravel() for x in np.meshgrid(*[np.arange(5)] * 3)], axis=1)
+    kvecs = np.stack([x.ravel() for x in np.meshgrid(*[np.arange(4)] * 3)], axis=1)
     kvecs = np.dot(kvecs, Gvecs)
     skacc = SkAccumulator(kvecs)
 
-    configs = np.zeros((1, 2, 3))
     configs = PeriodicConfigs(configs, Lvecs)
-    sk = skacc(configs, None)
+    sk = skacc(configs, None)["Sk"]
     skavg = skacc.avg(configs, None)
-    print(sk)
-    print(skavg)
+    print(np.round(sk, 4))
+    # print(skavg)
 
 
 if __name__ == "__main__":

a3ce16879c088650a202ffa55a166e48eb459d20
Lucas K. Wagner <lucas.wagner@gmail.com>
2020-05-28 09:47:14 -0500

    orthogonal optimization with better output.

--- pyqmc/optimize_orthogonal.py
+++ pyqmc/optimize_orthogonal.py
@@ -60,7 +60,7 @@ def sample_overlap(
     for block in range(nblocks):
         block_avg = {}
         for step in range(nsteps_per_block):
-            print("block", block, " step", step)
+            print("-", end='',flush=True)
             for e in range(nelec):
                 # Propose move
                 grads = [np.real(wf.gradient(e, configs.electron(e)).T) for wf in wfs]
@@ -148,6 +148,7 @@ def sample_overlap(
             if k not in return_data:
                 return_data[k] = np.zeros((nblocks, *it.shape))
             return_data[k][block, ...] = it.copy()
+    print("vmc done", flush=True)
     return return_data, configs
 
 

c555480ad6df6a41ba3f3f78dd8cebcde37abf70
Will Wheeler <willwheelera@gmail.com>
2020-07-10 18:44:38 -0500

    add functions keys() and shapes() to accumulators

--- pyqmc/tbdm.py
+++ pyqmc/tbdm.py
@@ -225,14 +225,21 @@ class TBDMAccumulator:
 
         return results
 
+    def keys(self):
+        return set(["value", "norm_a", "norm_b", "acceptance_a", "acceptance_b"])
+
+    def shapes(self):
+        d = {"value": (self._ijkl.shape[1],)}
+        for e, s in zip(["a", "b"], self._spin_sector):
+            d["norm_%s" % e] = (self._orb_coeff[s].shape[1],)
+            d["acceptance_%s" % e] = ()
+        return d
+
+    def information(self):
+        return {"ijkl": self._ijkl.T}
+
     def avg(self, configs, wf):
-        d = self(configs, wf)
-        davg = {}
-        for k, v in d.items():
-            # print(k, v.shape)
-            davg[k] = np.mean(v, axis=0)
-        davg["ijkl"] = self._ijkl.T
-        return davg
+        return {k: np.mean(it, axis=0) for k, it in self(configs, wf).items()}
 
     def get_extra_configs(self, configs):
         """ Returns an nstep length array of configurations
--- tests/unit/test_accumulators.py
+++ tests/unit/test_accumulators.py
@@ -1,6 +1,8 @@
 import numpy as np
 from pyqmc.energy import energy
 from pyqmc.accumulators import LinearTransform
+from pyqmc.obdm import OBDMAccumulator
+import pyqmc
 
 
 def test_transform():
@@ -8,7 +10,6 @@ def test_transform():
     TODO: figure out a thing to test.
     """
     from pyscf import gto, scf
-    import pyqmc
 
     r = 1.54 / 0.529177
     mol = gto.M(
@@ -34,5 +35,64 @@ def test_transform():
     assert gradtrans.shape[0] == nconfig
 
 
+def test_info_functions_mol():
+    from pyscf import gto, scf
+    from pyqmc.tbdm import TBDMAccumulator
+
+    mol = gto.Mole()
+    mol.atom = """He 0.00 0.00 0.00 """
+    mol.basis = "ccpvdz"
+    mol.build()
+
+    mf = scf.RHF(mol)
+    ehf = mf.kernel()
+
+    wf, to_opt = pyqmc.default_sj(mol, mf)
+    accumulators = {
+        "pgrad": pyqmc.gradient_generator(mol, wf, to_opt),
+        "obdm": OBDMAccumulator(mol, orb_coeff=mf.mo_coeff),
+        "tbdm_updown": TBDMAccumulator(mol, np.asarray([mf.mo_coeff] * 2), (0, 1)),
+    }
+    info_functions(mol, wf, accumulators)
+
+
+def test_info_functions_pbc():
+    from pyscf.pbc import gto, scf
+    from pyqmc.supercell import get_supercell
+
+    mol = gto.Cell(atom="He 0.00 0.00 0.00", basis="ccpvdz", unit="B")
+    mol.a = 5.61 * np.eye(3)
+    mol.build()
+
+    mf = scf.KRHF(mol, kpts=mol.make_kpts([2, 2, 2])).density_fit()
+    ehf = mf.kernel()
+
+    supercell = get_supercell(mol, 2 * np.eye(3))
+    kinds = [0, 1]
+    dm_orbs = [mf.mo_coeff[i][:, :2] for i in kinds]
+    wf, to_opt = pyqmc.default_sj(mol, mf)
+    accumulators = {
+        "pgrad": pyqmc.gradient_generator(mol, wf, to_opt, ewald_gmax=10),
+        "obdm": OBDMAccumulator(mol, dm_orbs, kpts=mf.kpts[kinds]),
+        "Sq": pyqmc.accumulators.SqAccumulator(mol.lattice_vectors()),
+    }
+    info_functions(mol, wf, accumulators)
+
+
+def info_functions(mol, wf, accumulators):
+    accumulators["energy"] = accumulators["pgrad"].enacc
+    configs = pyqmc.initial_guess(mol, 100)
+    wf.recompute(configs)
+    for k, acc in accumulators.items():
+        shapes = acc.shapes()
+        keys = acc.keys()
+        assert shapes.keys() == keys, "keys: {0}\nshapes: {1}".format(keys, shapes)
+        avg = acc.avg(configs, wf)
+        assert avg.keys() == keys, (k, avg.keys(), keys)
+        for ka in keys:
+            assert shapes[ka] == avg[ka].shape, "{0} {1}".format(ka, avg[ka].shape)
+
+
 if __name__ == "__main__":
-    test_transform()
+    test_info_functions_mol()
+    test_info_functions_pbc()

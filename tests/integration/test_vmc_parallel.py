cd3be1d7d8483247a5041091e23153559bd1e236
Lucas K. Wagner <lucas.wagner@gmail.com>
2020-07-17 09:30:47 -0500

    added a test for parallel execution.

---
+++ tests/integration/test_vmc_parallel.py
@@ -0,0 +1,29 @@
+import pyqmc.recipes
+import concurrent.futures
+import os 
+ncore = 2
+nconfig = ncore * 400
+
+def run_scf(chkfile):
+    from pyscf import gto, scf
+
+    mol = gto.M(
+        atom="H 0 0 0; H 0 0. 1.4", basis="ccecpccpvdz", ecp="ccecp", unit='bohr'
+    )
+    mf = scf.RHF(mol)
+    mf.chkfile=chkfile
+    mf.kernel()
+    
+
+def test_parallel():
+    #cluster = LocalCluster(n_workers=ncore, threads_per_worker=1)
+    #client = Client(cluster)
+    run_scf("h2.hdf5")
+    with concurrent.futures.ProcessPoolExecutor(max_workers=ncore) as client:
+        pyqmc.recipes.OPTIMIZE("h2.hdf5","linemin.hdf5", nconfig=50, client=client, npartitions=ncore)
+    assert os.path.isfile("linemin.hdf5")
+    os.remove("h2.hdf5")
+    os.remove("linemin.hdf5")
+
+    
+

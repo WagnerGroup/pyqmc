5f9ec865e577c0649dcab7254307eabcb7764af2
Will Wheeler <willwheelera@gmail.com>
2021-02-11 14:18:35 -0600

    make cupy import clearer

--- pyqmc/loadcupy.py
+++ pyqmc/loadcupy.py
@@ -2,15 +2,18 @@ try:
     import cupy as cp
     from cupy import get_array_module, asnumpy, fuse
 except ImportError:
-    import numpy as np
-
-    cp = np
+    print("using numpy instead of cupy")
+    import numpy as cp
 
     def get_array_module(a):
-        return np
+        return cp
 
     def asnumpy(a):
         return a
 
     def fuse():
         return lambda x: x
+
+
+finally:
+    print("cp is module", cp)

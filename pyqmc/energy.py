7935973d6314d89c124566d54c5dbe94b842a3db
Lucas K. Wagner <lucas.wagner@gmail.com>
2021-05-17 11:59:51 -0500

    fix circular import in recipes

--- pyqmc/energy.py
+++ pyqmc/energy.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pyqmc.eval_ecp as eval_ecp
-from pyqmc.distance import RawDistance
+import pyqmc.distance as distance
 
 
 def ee_energy(configs):
@@ -23,7 +23,7 @@ def ei_energy(mol, configs):
 
 
 def ii_energy(mol):
-    d = RawDistance()
+    d = distance.RawDistance()
     rij, ij = d.dist_matrix(mol.atom_coords()[np.newaxis, :, :])
     if len(ij) == 0:
         return np.array([0.0])
--- pyqmc/gpu.py
+++ pyqmc/gpu.py
@@ -1,13 +1,17 @@
 class NoGPUFoundError(Exception):
     pass
 
+
 try:
     import cupy as cp
     from cupy import get_array_module, asnumpy, fuse
+    from cupyx.scipy.special import erfc
+
     if not cp.cuda.is_available():
         raise NoGPUFoundError
 except (ModuleNotFoundError, NoGPUFoundError) as e:
     import numpy as cp
+    from scipy.special import erfc
 
     def get_array_module(a):
         return cp

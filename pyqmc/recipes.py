e59ae4c415e35c6d039ea9e28387f7c65bde780d
Will Wheeler <willwheelera@gmail.com>
2021-05-19 17:35:04 -0500

    pass recipe kwargs instead of dict for method kwargs

--- pyqmc/recipes.py
+++ pyqmc/recipes.py
@@ -5,14 +5,14 @@ import pyqmc.supercell as supercell
 import pyqmc.linemin as linemin
 import pyqmc.optimize_ortho as optimize_ortho
 import pyqmc.dmc as dmc
-import pyqmc.mc 
+import pyqmc.mc
 import pyqmc.reblock
 import numpy as np
 import h5py
 import scipy.stats
 import pandas as pd
 import copy
-import pyqmc.accumulators 
+import pyqmc.accumulators
 
 
 def OPTIMIZE(
@@ -23,14 +23,11 @@ def OPTIMIZE(
     ci_checkfile=None,
     start_from=None,
     S=None,
-    client=None,
-    npartitions=None,
     jastrow_kws=None,
     slater_kws=None,
-    linemin_kws=None,
+    **linemin_kws,
 ):
-    if linemin_kws is None:
-        linemin_kws = {}
+    linemin_kws["hdf_file"] = output
 
     target_root = 0
     if ci_checkfile is None:
@@ -52,29 +49,11 @@ def OPTIMIZE(
     configs = pyqmc.mc.initial_guess(mol, nconfig)
     acc = pyqmc.accumulators.gradient_generator(mol, wf, to_opt)
     if anchors is None:
-        linemin.line_minimization(
-            wf,
-            configs,
-            acc,
-            verbose=True,
-            hdf_file=output,
-            client=client,
-            npartitions=npartitions,
-            **linemin_kws
-        )
+        linemin.line_minimization(wf, configs, acc, **linemin_kws)
     else:
         wfs = [wftools.read_wf(copy.deepcopy(wf), a) for a in anchors]
         wfs.append(wf)
-        optimize_ortho.optimize_orthogonal(
-            wfs,
-            configs,
-            acc,
-            # verbose=True,
-            hdf_file=output,
-            client=client,
-            npartitions=npartitions,
-            **linemin_kws
-        )
+        optimize_ortho.optimize_orthogonal(wfs, configs, acc, **linemin_kws)
 
 
 def generate_accumulators(mol, mf, energy=True, rdm1=False, extra_accumulators=None):
@@ -113,15 +92,12 @@ def VMC(
     ci_checkfile=None,
     start_from=None,
     S=None,
-    client=None,
-    npartitions=None,
     jastrow_kws=None,
     slater_kws=None,
-    vmc_kws=None,
     accumulators=None,
+    **vmc_kws,
 ):
-    if vmc_kws is None:
-        vmc_kws = {}
+    vmc_kws["hdf_file"] = output
 
     target_root = 0
     if ci_checkfile is None:
@@ -150,11 +126,7 @@ def VMC(
         wf,
         configs,
         accumulators=generate_accumulators(mol, mf, **accumulators),
-        verbose=True,
-        hdf_file=output,
-        client=client,
-        npartitions=npartitions,
-        **vmc_kws
+        **vmc_kws,
     )
 
 
@@ -165,15 +137,12 @@ def DMC(
     start_from=None,
     ci_checkfile=None,
     S=None,
-    client=None,
-    npartitions=None,
     jastrow_kws=None,
     slater_kws=None,
-    dmc_kws=None,
     accumulators=None,
+    **dmc_kws,
 ):
-    if dmc_kws is None:
-        dmc_kws = {}
+    dmc_kws["hdf_file"] = output
 
     target_root = 0
     if ci_checkfile is None:
@@ -201,11 +170,7 @@ def DMC(
         wf,
         configs,
         accumulators=generate_accumulators(mol, mf, **accumulators),
-        verbose=True,
-        hdf_file=output,
-        client=client,
-        npartitions=npartitions,
-        **dmc_kws
+        **dmc_kws,
     )
 
 

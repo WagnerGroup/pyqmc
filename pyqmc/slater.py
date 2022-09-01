75cc5ae8529ee7f789b58b0fe7ad9ee32dccae33
Will Wheeler <willwheelera@gmail.com>
2022-09-01 16:07:11 -0500

    slater inverse should be initialized on gpu

--- pyqmc/slater.py
+++ pyqmc/slater.py
@@ -162,7 +162,7 @@ class Slater:
                 )
                 # print(configs.configs[])
                 print(f"zero {is_zero/np.prod(compute.shape)}")
-            self._inverse.append(np.zeros(mo_vals.shape, dtype=mo_vals.dtype))
+            self._inverse.append(gpu.cp.zeros(mo_vals.shape, dtype=mo_vals.dtype))
             for d in range(compute.shape[1]):
                 self._inverse[s][compute[:, d], d, :, :] = gpu.cp.linalg.inv(
                     mo_vals[compute[:, d], d, :, :]

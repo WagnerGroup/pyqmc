2ef903c053e0c15f9c5a4cd80bf7420edbfebd4d
nirvaan <nirvaan97@gmail.com>
2022-10-03 08:52:00 -0500

    written one spin case for testvalue

--- pyqmc/temp_test_three_body.py
+++ pyqmc/temp_test_three_body.py
@@ -6,6 +6,6 @@ import pyscf
 
 mol = pyscf.gto.M(atom="Li 0. 0. 0.; H 0. 0. 1.5", basis="sto-3g", unit="bohr")
 a_basis, b_basis = pyqmc.wftools.default_jastrow_basis(mol)
-J=pyqmc.three_body_jastrow.Three_Body_JastrowSpin(mol,a_basis,b_basis)
+J = pyqmc.three_body_jastrow.Three_Body_JastrowSpin(mol, a_basis, b_basis)
 configs = pyq.initial_guess(mol, 10)
 print(J.recompute(configs))

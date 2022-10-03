1b3dcd7697b61967470635b47f7cf54e5ac81d8c
Will Wheeler <willwheelera@gmail.com>
2022-10-03 15:40:37 -0500

    recompute using electron-partial; separate single_e_partial from testvalue

--- pyqmc/three_body_jastrow.py
+++ pyqmc/three_body_jastrow.py
@@ -19,20 +19,11 @@ class Three_Body_JastrowSpin:
 
     def recompute(self, configs):
         """returns phase, log value"""
-        self._configscurrent=configs.copy()
+        self._configscurrent = configs.copy()
         nconf, nelec = configs.configs.shape[:2]
+        na, nb = len(self.a_basis), len(self.b_basis)
         # n_elec in first axis to match di format
-        self.sum_j = np.zeros(
-            (
-                nelec,
-                nconf,
-                self._mol.natm,
-                len(self.a_basis),
-                len(self.a_basis),
-                len(self.b_basis),
-                2,
-            )
-        )
+        self.sum_j = np.zeros((nelec, nconf, self._mol.natm, na, na, nb, 2))
         # order of spin channel: upup,updown,downdown
 
         # electron-electron distances
@@ -69,197 +60,127 @@ class Three_Body_JastrowSpin:
             inds = tuple(zip(*ij_all[s]))
             b_2d_values_s[inds] = bvalues.swapaxes(0, 1)
             b_2d_values.append(b_2d_values_s)
-           
-        # evaluate a_values
-        # a_values are a evaluations ak(rIi)
+
+        # evaluate a_values ak(rIi)
         # might not need all of these, but have them defined here for now. idealy use as few as possible
         a_values = np.zeros((self._nelec, nconf, self._mol.natm, len(self.a_basis)))
         for i, a in enumerate(self.a_basis):
             # di dim nconf,I,nelec
             a_values[:, :, :, i] = a.value(di, ri)
 
-        self.sum_ij = np.zeros(
-            (
-                nconf,
-                self._mol.natm,
-                len(self.a_basis),
-                len(self.a_basis),
-                len(self.b_basis),
-                3,
-            )
-        )
-        self.sum_ij[:, :, :, :, :, 0] = np.einsum(
-            "inIk,jnIl,ijnm->nIklm", a_values[:nup], a_values[:nup], b_2d_values[0]
-        )
-        self.sum_ij[:, :, :, :, :, 1] = np.einsum(
-            "inIk,jnIl,ijnm->nIklm", a_values[:nup], a_values[nup:], b_2d_values[1]
-        )
-        self.sum_ij[:, :, :, :, :, 2] = np.einsum(
-            "inIk,jnIl,ijnm->nIklm", a_values[nup:], a_values[nup:], b_2d_values[2]
-        )
-        self.sum_j = np.zeros((nelec,nconf,self._mol.natm,len(self.a_basis),len(self.a_basis),len(self.b_basis),2))
-
         self.C = self.parameters["ccoeff"] + self.parameters["ccoeff"].swapaxes(1, 2)
-        val = np.einsum("Iklms,nIklms->n", self.C, self.sum_ij)
+
+        self.sum_jC = np.zeros((nelec, nconf))
+        updown = np.einsum(
+            "inIk,jnIl,ijnm,Iklm->ijn",
+            a_values[:nup],
+            a_values[nup:],
+            b_2d_values[1],
+            self.C[..., 1],
+        )
+        self.sum_jC[:nup] += updown.sum(axis=1)
+        self.sum_jC[nup:] += updown.sum(axis=0)
+        self.sum_jC[:nup] += np.einsum(
+            "inIk,jnIl,ijnm,Iklm->in",
+            a_values[:nup],
+            a_values[:nup],
+            b_2d_values[0],
+            self.C[..., 0],
+        )
+        self.sum_jC[nup:] += np.einsum(
+            "inIk,jnIl,ijnm,Iklm->in",
+            a_values[nup:],
+            a_values[nup:],
+            b_2d_values[2],
+            self.C[..., 2],
+        )
+
+        val = self.sum_jC.sum(axis=0)
         self.val = val
 
         return (np.ones(len(val)), val)
 
     def updateinternals(self, e, epos, configs, mask=None, saved_values=None):
-        nconf = configs.configs.shape[0]
-        mask = [True]*nconf
-        self._configscurrent.move(e, epos, mask)
-        
-        self.val = np.log(self.testvalue(e,epos)[0])+self.val
-
-    # nconf, nelec = configs.configs.shape[:2]
-    # #calculate new sum overj and set it
-    # nup = self._mol.nelec[0]
-    # ndown=self._mol.nelec[1]
-
-    # configs[:,e,:] = epos
-    # #di has shape nconf,natoms,3
-    # di = np.zeros((nelec, nconf, self._mol.natm, 3))
-    # for j in range(nelec):
-    #     di[j] = np.asarray(
-    #         configs.dist.dist_i(self._mol.atom_coords(), configs.configs[:, j, :])
-    #     )
-    # ri = np.linalg.norm(di, axis=-1)
-
-    # diup = di[:nup]
-    # didown=di[nup:]
-    # riup = ri[:nup]
-    # ridown=ri[nup:]
-
-    # #d has shape configs,nelec-1,3
-    # not_e = np.arange(self._nelec) != e
-
-    # d = configs.dist.dist_i(configs[:,not_e,...],epos)
-    # d_swapped = np.swapaxes(d,0,1)
-    # d_up_swapped = d_swapped[:nup]
-    # d_down_swapped = d_swapped[nup:]
-    # r_swapped= np.linalg.norm(d_swapped, axis=-1)
-    # r_up_swapped = r_swapped[:nup]
-    # r_down_swapped = r_swapped[nup:]
-
-    # is_up = e<nup
-
-    # a_up_values = np.zeros((nup,nconf,self._mol.natm,len(self.a_basis)))
-    # a_down_values = np.zeros((ndown,nconf,self._mol.natm,len(self.a_basis)))
-    # b_upup_values = np.zeros((self.nup*(self.nup-1)/2,nconf,len(self.b_basis)))
-    # b_updown_values = np.zeros((self.nup*self.ndown,nconf,len(self.b_basis)))
-    # b_downdown_values = np.zeros((self.ndown*(self.ndown-1)/2,nconf,len(self.b_basis)))
-    # # a_i_values = np.zeros(nconf,self._mol.natm,len(self.a_basis))
-
-    # # for i, a in enumerate(self.a_basis):
-    # #   a_i_values[:,:,i] = a.value(di,ri)
-    # #can probably remove this if and replace with integer stuff. but will do that last since it decreases readability for me
-
-    # if is_up:
-    #   sep = nup-1
-    #   for i, a in enumerate(self.a_basis):
-    #     a_up_values[e,:,:,i]=a.value(di,ri)
-    #   for j, b in enumerate(self.b_basis):
-    #     #unflatten i,j electron axes for this to work. Also think of way such that you dont have to unflatten it.
-    #     b_updown_values[e,:e,:,j] = b.value(d_down_swapped,r_down_swapped)[:e]
-    #     b_updown_values[e,e+1:,:,j] = b.value(d_down_swapped,r_down_swapped)[e:]
-    #     b_upup_values[e,:,:,j]= b.value(d_up_swapped,r_up_swapped)
-    # else:
-    #   sep=nup
-    #   for i, a in enumerate(self.a_basis):
-    #     a_down_values[e-nup,:,:,i]=a.value(di,ri)
-    #   for j, b in enumerate(self.b_basis):
-    #     b_updown_values[e,:,:,j] = b.value(d_down_swapped,r_down_swapped)
-    #     b_upup_values[e,:,:,j]= b.value(d_up_swapped,r_up_swapped)
-
-    # old_sum_e = self.sum_j[e]
-    # self.sum_j[e,:,:,:,:,:,0] = np.einsum('',a_values[e],a_up_values,b_values)
-    # old_sum_ij = self.sum_ij
-    # self.sum_ij =  old_sum_ij - old_sum_e + self.sum_j[e]
+        newconfigs = configs.copy()
+        newconfigs.move(e, epos, np.ones(len(epos.configs), dtype=bool))
+        self.recompute(newconfigs)
 
     def value(self):
-        # val = np.dot(self.parameters["coeff"], self.sumij)
         val = self.val
 
         return (np.ones(len(val)), val)
 
+    def single_e_partial(self, configs, e, epos, a_values):
+        nup = int(self._mol.nelec[0])
+        ndown = int(self._mol.nelec[1])
+        sep = nup - int(e < nup)
+        not_e = np.arange(self._nelec) != e
+
+        nconf = configs.configs.shape[0]
+        edown = int(e >= self._mol.nelec[0])
+
+        de = configs.dist.dist_i(configs.configs[:, not_e], epos)
+        re = np.linalg.norm(de, axis=-1)
+
+        di_e = configs.dist.dist_i(self._mol.atom_coords(), epos)
+        ri_e = np.linalg.norm(di_e, axis=-1)
+
+        ae = np.zeros((nconf, self._mol.natm, len(self.a_basis)))
+        for i, a in enumerate(self.a_basis):
+            ae[:, :, i] = a.value(di_e, ri_e)
+
+        b_values = np.zeros((self._nelec - 1, nconf, len(self.b_basis)))
+
+        for i, b in enumerate(self.b_basis):
+            # swap axes: nconf and nelec. for now doing it here.
+            b_values[:, :, i] = b.value(de, re).swapaxes(0, 1)
+
+        na, nb = len(self.a_basis), len(self.b_basis)
+        e_partial = np.zeros(nconf)
+        e_partial += np.einsum(
+            "nIk,jnIl,jnm,Iklm->n",
+            ae,
+            a_values[:sep],
+            b_values[:sep],
+            self.C[..., edown],
+        )
+        e_partial += np.einsum(
+            "nIk,jnIl,jnm,Iklm->n",
+            ae,
+            a_values[sep:],
+            b_values[sep:],
+            self.C[..., edown + 1],
+        )
+        return e_partial
+
     def testvalue(self, e, epos, mask=None):
-        epos=epos.configs
         configs = self._configscurrent
         nconf, nelec = configs.configs.shape[:2]
-        #edown is 1 if e is down spin and 0 for up spin
         edown = int(e >= self._mol.nelec[0])
-        epos_old = configs.configs[:, e, :]
-        rpos_old = np.linalg.norm(epos_old, axis=-1)
 
         nup = int(self._mol.nelec[0])
         ndown = int(self._mol.nelec[1])
-        #sep keeps track of how many up electrons are left if you dont include e. nup-1 if e is up, else it is nup
         sep = nup - int(e < nup)
-        #boolean array of size nelec. True for all indices except False for e.
         not_e = np.arange(self._nelec) != e
 
-        #de_old = np.zeros((nconf, np.sum(self._mol.nelec), 3))
-        de_old = configs.dist.dist_i(
-            configs.configs[:, not_e, :], configs.configs[:, e, :]
+        di = np.stack(
+            [
+                configs.dist.dist_i(self._mol.atom_coords(), configs.configs[:, e_, :])
+                for e_ in np.arange(nelec)[not_e]
+            ],
+            axis=0,
         )
-        re_old = np.linalg.norm(de_old, axis=-1)
 
-        #de_new = np.zeros((nconf, np.sum(self._mol.nelec), 3))
-        de_new = configs.dist.dist_i(configs.configs[:, not_e, :], epos)
-        re_new = np.linalg.norm(de_new, axis=-1)
-
-        di = np.zeros((nelec, nconf, self._mol.natm, 3))
-        # di_e = np.zeros((nconf, self._mol.natm, 3))
-        di_e = configs.dist.dist_i(self._mol.atom_coords(), epos)
-        for e in range(nelec):
-            di[e] = np.asarray(
-                configs.dist.dist_i(self._mol.atom_coords(), configs.configs[:, e, :])
-            )
-
-        ri_e = np.linalg.norm(di_e, axis=-1)
         ri = np.linalg.norm(di, axis=-1)
-        rpos = np.linalg.norm(epos, axis=-1)
 
-        a_values = np.zeros((self._nelec, nconf, self._mol.natm, len(self.a_basis)))
-        ae_new = np.zeros((nconf, self._mol.natm, len(self.a_basis)))
-        #ae_old = np.zeros((nconf, self._mol.natm, len(self.a_basis)))
+        a_values = np.zeros((self._nelec - 1, nconf, self._mol.natm, len(self.a_basis)))
 
         for i, a in enumerate(self.a_basis):
             # di dim nconf,I,nelec
             a_values[:, :, :, i] = a.value(di, ri)
-            ae_new[:, :, i] = a.value(di_e, ri_e)
 
-        ae_old = a_values[e]
-        a_values_not_e = a_values[not_e]
-        a_values_up = a_values_not_e[:sep,...]
-        a_values_down= a_values_not_e[sep:,...]
+        e_partial_old = self.single_e_partial(configs, e, configs.configs[:, e], a_values)
+        e_partial_new = self.single_e_partial(configs, e, epos.configs, a_values)
 
-        b_values_old = np.zeros((self._nelec-1, nconf, len(self.b_basis)))
-        b_values_new = np.zeros((self._nelec-1, nconf, len(self.b_basis)))
-
-        for i, b in enumerate(self.b_basis):
-            # swap axes: nconf and nelec. for now doing it here.
-            b_values_old[:, :, i] = b.value(de_old, re_old).swapaxes(0,1)
-            b_values_new[:, :, i] = b.value(de_new, re_new).swapaxes(0,1)
-
-        b_up_old = b_values_old[:sep,...]
-        b_down_old = b_values_old[sep:,...]
-        b_up_new = b_values_new[:sep,...]
-        b_down_new = b_values_new[sep:,...]
-        
-        e_partial_old = np.zeros((nconf,self._mol.natm,len(self.a_basis),len(self.a_basis),len(self.b_basis),3))
-        e_partial_new = np.zeros((nconf,self._mol.natm,len(self.a_basis),len(self.a_basis),len(self.b_basis),3))
-        
-        #if e is down, then store in updown stream(index =1). if e is up store in upup stream(index=0).
-        e_partial_old[...,edown] = np.einsum("nIk,jnIl,jnm->nIklm", ae_old, a_values_up, b_up_old)
-        e_partial_new[...,edown] = np.einsum("nIk,jnIl,jnm->nIklm", ae_new, a_values_up, b_up_new)
-        
-        #if e is down, store in downdown stream (index=2), if e is up store in updown stream(index=1)
-        e_partial_old[...,edown+1] =np.einsum("nIk,jnIl,jnm->nIklm", ae_old, a_values_down, b_down_old)
-        e_partial_new[...,edown+1] =np.einsum("nIk,jnIl,jnm->nIklm", ae_new, a_values_down, b_down_new)
-
-        delta_sum_ij = 2 * e_partial_new - 2 * e_partial_old
-        value_diff = np.einsum('nIklms,Iklms->n',delta_sum_ij, self.parameters["ccoeff"])
-        val = np.exp(value_diff)
-        return val, None
+        self.val = np.exp(2 * e_partial_new - 2 * e_partial_old)
+        return self.val, None

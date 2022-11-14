ad7c330ce017e6f74a8e35f806e9743d854c1791
nirvaan <nirvaan97@gmail.com>
2022-11-14 11:42:34 -0600

    added integration test for three body jastrow

--- tests/integration/test_line_minimization.py
+++ tests/integration/test_line_minimization.py
@@ -4,7 +4,14 @@ os.environ["MKL_NUM_THREADS"] = "1"
 os.environ["NUMEXPR_NUM_THREADS"] = "1"
 os.environ["OMP_NUM_THREADS"] = "1"
 import pandas as pd
-from pyqmc.api import generate_wf, line_minimization, initial_guess, gradient_generator
+from pyqmc.api import (
+    generate_wf,
+    generate_jastrow,
+    line_minimization,
+    initial_guess,
+    gradient_generator,
+)
+import pyqmc.wftools as wftools
 import pytest
 
 
@@ -26,5 +33,28 @@ def test_linemin(H2_ccecp_uhf):
     assert mfen > enfinal
 
 
+@pytest.mark.slow
+def test_linemin_three_body(H2_ccecp_uhf):
+    """Optimize a Slater-3bodyJastrow wave function and check that it's better than Hartree-Fock"""
+    mol, mf = H2_ccecp_uhf
+    wf, to_opt = generate_wf(
+        mol,
+        mf,
+        jastrow=[generate_jastrow, wftools.generate_3_jastrow],
+        jastrow_kws=[{}, {}],
+    )
+    nconf = 100
+    wf, dfgrad = line_minimization(
+        wf, initial_guess(mol, nconf), gradient_generator(mol, wf, to_opt)
+    )
+
+    dfgrad = pd.DataFrame(dfgrad)
+    mfen = mf.energy_tot()
+    enfinal = dfgrad["energy"].values[-1]
+    enfinal_err = dfgrad["energy_error"].values[-1]
+    assert mfen > enfinal
+
+
 if __name__ == "__main__":
     test_linemin()
+    test_linemin_three_body()

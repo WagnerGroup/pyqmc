0fdb631739ec05f2057d9570b7efc15acf95ea12
nirvaank <nirvaan97@gmail.com>
2022-11-29 13:29:08 -0600

    black test_line_min

--- tests/integration/test_line_minimization.py
+++ tests/integration/test_line_minimization.py
@@ -4,7 +4,7 @@ os.environ["MKL_NUM_THREADS"] = "1"
 os.environ["NUMEXPR_NUM_THREADS"] = "1"
 os.environ["OMP_NUM_THREADS"] = "1"
 import pandas as pd
-from pyqmc.api import generate_wf,line_minimization,initial_guess,gradient_generator
+from pyqmc.api import generate_wf, line_minimization, initial_guess, gradient_generator
 import pytest
 
 

3075ca57fc5b2d872d51900326cb8a62093a0041
nirvaank <nirvaan97@gmail.com>
2022-12-10 17:33:36 -0600

    added docstring to test_distance

--- tests/unit/test_distance.py
+++ tests/unit/test_distance.py
@@ -2,11 +2,16 @@ import pyqmc.mc as mc
 
 
 def test_distance(LiH_sto3g_uhf):
+    """ dupdown is the (nup,ndown) shaped array of electron distances between up spin and down spin electrons
+    ij_updown is a list of tuples (i,j) where i<nup, j<ndown 
+    each element (i,j) of ij_updown indicates that the corresponding element of dupdown is the distance between jth component of
+    configs2 and ith component of configs1
+    This test checks whether this is true.
+    """
     mol, mf = LiH_sto3g_uhf
     nconfigs = 10
     configs = mc.initial_guess(mol, nconfigs)
     nup = mol.nelec[0]
-    # dupdown is the (nup,ndown) shaped array of electron distances between up spin and down spin electrons
     configs1 = configs.configs[:, :nup]
     configs2 = configs.configs[:, nup:]
 

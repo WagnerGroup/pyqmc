5a64d2cce933088ba00178ad178fe55c08938310
Lucas Wagner <lucas.wagner@gmail.com>
2023-02-04 16:24:30 -0600

    Added lagrange multiplier

--- examples/he.py
+++ examples/he.py
@@ -1,23 +1,23 @@
 if __name__ == "__main__":
     import pyscf
-    import pyqmc
+    import pyqmc.api as pyq
 
     mol = pyscf.gto.M(atom="He 0. 0. 0.", basis="bfd_vdz", ecp="bfd", unit="bohr")
 
     mf = pyscf.scf.RHF(mol).run()
-    wf, to_opt = pyqmc.default_sj(mol, mf)
+    wf, to_opt = pyq.generate_wf(mol, mf)
 
     nconfig = 1000
-    configs = pyqmc.initial_guess(mol, nconfig)
+    configs = pyq.initial_guess(mol, nconfig)
 
-    acc = pyqmc.gradient_generator(mol, wf, to_opt)
-    pyqmc.line_minimization(wf, configs, acc, hdf_file="he_opt.hdf5", verbose=True)
-
-    pyqmc.rundmc(
+    acc = pyq.gradient_generator(mol, wf, to_opt)
+    pyq.line_minimization(wf, configs, acc,  verbose=True, max_iterations=10)
+    quit()
+    pyq.rundmc(
         wf,
         configs,
         nsteps=5000,
-        accumulators={"energy": pyqmc.EnergyAccumulator(mol)},
+        accumulators={"energy": pyq.EnergyAccumulator(mol)},
         tstep=0.02,
         hdf_file="he_dmc.hdf5",
         verbose=True,

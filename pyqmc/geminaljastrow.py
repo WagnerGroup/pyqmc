096a28d41ec00e113eaa7a30837cbcbf226c9ae2
Will Wheeler <willwheelera@gmail.com>
2023-06-08 11:35:29 -0500

    add more GeminalJastrow documentation

--- pyqmc/geminaljastrow.py
+++ pyqmc/geminaljastrow.py
@@ -8,44 +8,28 @@ class GeminalJastrow:
     r"""
     Jastrow factor defined by Casula, Attaccalite, and Sorella, J. Chem. Phys. 121, 7110 (2004); https://doi.org/10.1063/1.1794632
 
-    ..math:: \widetilde{J}_G(\mathbf{R}) = \sum_{i\ne j} \sum_{mn} g_{mn} \chi_m(\mathbf{r}_i) \chi_n(\mathbf{r}_j)
+    .. math:: J_G(\mathbf{R}) = \sum_{i\ne j} \sum_{mn} \tilde g_{mn} \chi_m(\mathbf{r}_i) \chi_n(\mathbf{r}_j)
 
-    :math:`\chi_m(\mathbf{r})` is a set of basis orbitals
+    :math:`\chi_m(\mathbf{r})` is a set of basis orbitals.
 
-    There is a redundancy since :math:`\chi_m(\mathbf{r}_i) \chi_n(\mathbf{r}_j) = \chi_n(\mathbf{r}_j) \chi_m(\mathbf{r}_i)`, so the effective coefficient is :math:`g_{mn} + g_{nm}`.
-    It is important to remove the extra degree of freedom in :math:`g_{mn}` to be able to optimize the function.
-    This can be done by summing only over indices :math:`i<j` or :math:`m\le n`.
-    Under the second constraint, the case of :math:`m=n` will count the pair :math:`(i,j)` twice, whereas it will only be conuted once under the first constraint.
-    This factor of two can be absorbed into the parameter :math:`g_{mm}` and doesn't give rise to an extra degree of freedom, so it's not a problem.
-    
-    Jastrow factors are required to be symmetric under particle exchange.
-    So the term :math:`\chi_m(\mathbf{r}_i) \chi_n(\mathbf{r}_j)` must appear together with its exchanged version :math:`\chi_m(\mathbf{r}_j) \chi_n(\mathbf{r}_i)`.
-    Either of the above index restrictions :math:`i<j` or :math:`m\le n` satisfies this symmetry requirement.
-    It is obvious that the restriction :math:`m\le n` satisfies this symmetry requirement, since we are explicitly including both of these terms in the sum.
-    The restriction :math:`i<j` is equivalent. Starting from the :math:`m\le n` form, we can take all terms having :math:`j<i` and exchange the two orbitals in the product so that :math:`m\ge n` and :math:`i<j:. 
-    
-    
-    ..math:: J_G(\mathbf{R}) = \sum_{m\le n} \sum_{i\ne j} g_{mn} \chi_m(\mathbf{r}_i) \chi_n(\mathbf{r}_j)
+    We can split the sum over all :math:`i, j` into two sums over all pairs :math:`i<j` (the second term exchanges all pairs of particles from the first, :math:`\mathbf{r}_i \leftrightarrow \mathbf{r}_j`)
 
-    ..math:: J_G(\mathbf{R}) = \sum_{m\le n} \sum_{i< j} g_{mn} \chi_m(\mathbf{r}_i) \chi_n(\mathbf{r}_j)
-                             + \sum_{m\le n} \sum_{i> j} g_{mn} \chi_m(\mathbf{r}_i) \chi_n(\mathbf{r}_j)
+    .. math:: J_G(\mathbf{R}) = 
+        \underbrace{\sum_{i< j} \sum_{mn} \tilde g_{mn} \chi_m(\mathbf{r}_i) \chi_n(\mathbf{r}_j)}_{J_G^1(\mathbf{R})}
+      + \underbrace{\sum_{i< j} \sum_{mn} \tilde g_{mn} \chi_m(\mathbf{r}_j) \chi_n(\mathbf{r}_i)}_{J_G^2(\mathbf{R})}
 
-    ..math:: J_G(\mathbf{R}) = \sum_{m\le n} \sum_{i< j} g_{mn} \chi_m(\mathbf{r}_i) \chi_n(\mathbf{r}_j)
-                             + \sum_{m\le n} \sum_{i< j} g_{mn} \chi_m(\mathbf{r}_j) \chi_n(\mathbf{r}_i)
+    Reordering the product in :math:`J_G^2` and reindexing :math:`m\leftrightarrow n` yields
 
-    ..math:: J_G(\mathbf{R}) = \sum_{m\le n} \sum_{i< j} g_{mn} \chi_m(\mathbf{r}_i) \chi_n(\mathbf{r}_j)
-                             + \sum_{m\le n} \sum_{i< j} g_{mn} \chi_n(\mathbf{r}_i) \chi_m(\mathbf{r}_j)
+    .. math:: J_G^2(\mathbf{R}) = \sum_{i< j} \sum_{mn} \tilde g_{nm} \chi_m(\mathbf{r}_i) \chi_n(\mathbf{r}_j),
 
-    ..math:: J_G(\mathbf{R}) = \sum_{m\le n} \sum_{i< j} g_{mn} \chi_m(\mathbf{r}_i) \chi_n(\mathbf{r}_j)
-                             + \sum_{m\ge n} \sum_{i< j} g_{mn} \chi_m(\mathbf{r}_i) \chi_n(\mathbf{r}_j)
+    showing that exchanging two electrons is equivalent to exchanging the indices of :math:`\tilde g_{mn}`. 
+    The coefficient matrix :math:`\tilde g_{mn}` has extra degrees of freedom: only the symmetric part contributes.
 
-    ..math:: J_G(\mathbf{R}) = \sum_{mn} \sum_{i< j} g_{mn} \chi_m(\mathbf{r}_i) \chi_n(\mathbf{r}_j)
-                             + \sum_{m} \sum_{i< j} g_{mm} \chi_m(\mathbf{r}_i) \chi_m(\mathbf{r}_j)
+    .. math:: J_G(\mathbf{R}) = \sum_{i< j} \sum_{mn} (\tilde g_{mn}+\tilde g_{nm}) \chi_m(\mathbf{r}_i) \chi_n(\mathbf{r}_j)
 
-    ..math:: J_G(\mathbf{R}) = \sum_{i<j} \sum_{mn} g_{mn} \chi_m(\mathbf{r}_i) \chi_n(\mathbf{r}_j)
+    .. math:: J_G(\mathbf{R}) = \sum_{i< j} \sum_{m\le n} g_{mn} \chi_m(\mathbf{r}_i) \chi_n(\mathbf{r}_j)
 
-    In both cases, we need to evaluate every orbital at the position of all electrons, so neither form appears preferable for efficiency.
-    However, the case :math:`m\le n` contains about half the parameters, meaning that the :math:`S_{ij}` matrix formed during optimization is four times smaller, so the :math:`m\le n` choice is preferable.
+    The independent parameters are :math:`g_{mn} = \tilde g_{mn} + \tilde g_{nm}, \; m \le n`.
     """
 
     def __init__(self, mol, orbitals=None):
@@ -65,19 +49,29 @@ class GeminalJastrow:
             self.orbitals = orbitals
         randpos = np.random.random((1, 3))
         dim = mol.eval_gto("GTOval_cart", randpos).shape[-1]
-        self.parameters = {"gcoeff": gpu.cp.zeros((dim, dim))}
+        self.parameters = {"gcoeff": gpu.cp.zeros(int(dim*(dim+1)/2))}
         self.dtype = float
         self.optimize = "greedy"
 
     def recompute(self, configs):
+        r"""
+        Initializes the 2D array :math:`g_{mn}` from the 1D parameter vector
+        """
         nconf, self.nelec = configs.configs.shape[:2]
         # shape of arrays:
         # ao_val: (nconf, nelec, nbasis)
         aos = self.orbitals.aos("GTOval_cart", configs)
         self.ao_val = aos.reshape(nconf, self.nelec, aos.shape[-1])
+        self.gcoeff = gpu.cp.zeros((aos.shape[-1], aos.shape[-1]))
+        triu_inds = gpu.cp.triu_indices(aos.shape[-1])
+        self.gcoeff[triu_inds] = self.parameters["gcoeff"]
+        
         return self.value()
 
     def updateinternals(self, e, epos, configs, mask=None, saved_values=None):
+        """
+        AO values are saved (nconfig, nelec, nao)
+        """
         if mask is None:
             mask = [True] * epos.configs.shape[0]
         if saved_values is None:
@@ -87,10 +81,15 @@ class GeminalJastrow:
         self.ao_val[mask, e, :] = aoval
 
     def value(self):
+        r"""
+        .. math:: \sum_{m\le n} \sum_{i<j} g_{mn} \chi_m(\mathbf{r}_i) \chi_n(\mathbf{r}_j)
+
+        :math:`g_{mn}` only has nonzero elements where :math:`m\le n`.
+        """
         mask = gpu.cp.tril(gpu.cp.ones((self.nelec, self.nelec)), -1)
         vals = gpu.cp.einsum(
             "mn,cim, cjn, ij-> c",
-            self.parameters["gcoeff"],
+            self.gcoeff,
             self.ao_val,
             self.ao_val,
             mask,
@@ -99,18 +98,35 @@ class GeminalJastrow:
         signs = np.ones(len(vals))
         return (signs, gpu.asnumpy(vals))
 
-    def compute_value(self, ao_e, ao, e):
+    def _compute_value(self, ao_e, ao, e):
+        r"""
+        .. math:: \sum_{m\le n} \sum_{i<e} g_{mn} \chi_m(\mathbf{r}_e) \chi_n(\mathbf{r}_i)
+                             + \sum_{e<j} g_{mn} \chi_n(\mathbf{r}_e) \chi_m(\mathbf{r}_j)
+
+        For derivatives, 
+
+        .. math:: \sum_{m\le n} \sum_{i<e} g_{mn} \nabla\chi_m(\mathbf{r}_e) \chi_n(\mathbf{r}_i)
+                             + \sum_{e<j} g_{mn} \nabla\chi_n(\mathbf{r}_e) \chi_m(\mathbf{r}_j)
+
+        :math:`g_{mn}` only has nonzero elements where :math:`m\le n`.
+
+        Parameters:
+        :parameter ndarray(nconfig, nao) ao_e: ao values or derivatives of electron e. If derivatives, shape is (nderiv, nconfig, nao)
+        :parameter ndarray(nconfig, nelec, nao) ao: ao values of all electrons.
+        :parameter int e: electron index
+
+        """
         # `...` is for derivatives
         curr_val = gpu.cp.einsum(
             "mn, ...cm, cjn -> ...c",
-            self.parameters["gcoeff"],
+            self.gcoeff,
             ao_e,
             ao[:, :e, :],
             optimize=self.optimize,
         )
         curr_val += gpu.cp.einsum(
             "mn, ...cn, cim -> ...c",
-            self.parameters["gcoeff"],
+            self.gcoeff,
             ao_e,
             ao[:, e + 1 :, :],
             optimize=self.optimize,
@@ -119,43 +135,67 @@ class GeminalJastrow:
 
     def gradient_value(self, e, epos):
         ao = self.orbitals.aos("GTOval_cart_deriv1", epos)[0]
-        deriv = self.compute_value(ao, self.ao_val, e)
-        curr_val = self.compute_value(self.ao_val[:, e], self.ao_val, e)
+        deriv = self._compute_value(ao, self.ao_val, e)
+        curr_val = self._compute_value(self.ao_val[:, e], self.ao_val, e)
         val_ratio = gpu.cp.exp(deriv[0] - curr_val)
         return gpu.asnumpy(deriv[1:]), gpu.asnumpy(val_ratio), ao[0]
 
     def gradient(self, e, epos):
+        r"""
+        .. math:: \sum_{m\le n} \sum_{i<e} g_{mn} \nabla\chi_m(\mathbf{r}'_e) \chi_n(\mathbf{r}_i)
+                             + \sum_{e<j} g_{mn} \nabla\chi_n(\mathbf{r}'_e) \chi_m(\mathbf{r}_j)
+        """
+
         ao = self.orbitals.aos("GTOval_cart_deriv1", epos)[0]
-        grad = self.compute_value(ao[1:], self.ao_val, e)
+        grad = self._compute_value(ao[1:], self.ao_val, e)
         return gpu.asnumpy(grad)
 
     def laplacian(self, e, epos):
         return self.gradient_laplacian(e, epos)[1]
 
     def gradient_laplacian(self, e, epos):
+        r"""
+        .. math:: \nabla_e J_G = \sum_{m\le n} \sum_{i<e} g_{mn} \nabla\chi_m(\mathbf{r}'_e) \chi_n(\mathbf{r}_i)
+                             + \sum_{e<j} g_{mn} \nabla\chi_n(\mathbf{r}'_e) \chi_m(\mathbf{r}_j)
+        .. math:: \nabla_e^2 J_G = \sum_{m\le n} \sum_{i<e} g_{mn} \nabla^2\chi_m(\mathbf{r}'_e) \chi_n(\mathbf{r}_i)
+                             + \sum_{e<j} g_{mn} \nabla^2\chi_n(\mathbf{r}'_e) \chi_m(\mathbf{r}_j)
+        .. math:: \nabla_e^2 e^{J_G(\mathbf{R})} / e^{J_G(\mathbf{R})} = \nabla_e^2 J_G + |\nabla_e J_G|^2
+        """
         ao = self.orbitals.aos("GTOval_cart_deriv2", epos)[0]
         ao = gpu.cp.concatenate(
             [ao[1:4, ...], ao[[4, 7, 9], ...].sum(axis=0, keepdims=True)], axis=0
         )
-        deriv = self.compute_value(ao, self.ao_val, e)
+        deriv = self._compute_value(ao, self.ao_val, e)
         grad = deriv[:3]
         lap3 = gpu.cp.einsum("dc,dc->c", grad, grad)
         return gpu.asnumpy(grad), gpu.asnumpy(deriv[3] + lap3)
 
     def pgradient(self):
+        r"""
+        .. math:: \frac{\partial J_G}{\partial g_{mn}} = \sum_{i<j} \chi_m(\mathbf{r}_i) \chi_n(\mathbf{r}_j)
+
+        The upper triangular entries are extracted as a 1D array to match the shape of parameters["gcoeff"].
+        """
         mask = gpu.cp.tril(
             gpu.cp.ones((self.nelec, self.nelec)), -1
         )  # to prevent double counting of electron pairs
         coeff_grad = gpu.cp.einsum(
             "cim, cjn, ij-> cmn", self.ao_val, self.ao_val, mask, optimize=self.optimize
         )
-        return {"gcoeff": coeff_grad}
+        tui = gpu.cp.triu_indices(coeff_grad.shape[-1]) # select only m <= n
+        return {"gcoeff": coeff_grad[:, tui[0], tui[1]]}
 
     def testvalue(self, e, epos, mask=None):
+        r"""
+        .. math:: \sum_{m\le n} \sum_{i<e} g_{mn} \chi_m(\mathbf{r}'_e) \chi_n(\mathbf{r}_i)
+                             + \sum_{e<j} g_{mn} \chi_n(\mathbf{r}'_e) \chi_m(\mathbf{r}_j)
+                 -\left(\sum_{m\le n} \sum_{i<e} g_{mn} \chi_m(\mathbf{r}_e) \chi_n(\mathbf{r}_i)
+                             + \sum_{e<j} g_{mn} \chi_n(\mathbf{r}_e) \chi_m(\mathbf{r}_j)\right)
+        """
         if mask is None:
             mask = [True] * self.ao_val.shape[0]
         masked_ao_val = self.ao_val[mask]
-        curr_val = self.compute_value(masked_ao_val[:, e], masked_ao_val, e)
+        curr_val = self._compute_value(masked_ao_val[:, e], masked_ao_val, e)
         aos = self.orbitals.aos("GTOval_cart", epos, mask=mask)[0]
         new_ao_val = aos.reshape(
             len(curr_val), *epos.configs.shape[1:-1], aos.shape[-1]
@@ -163,14 +203,14 @@ class GeminalJastrow:
         # `...` is for extra dimension for ECP aux coordinates
         new_val = gpu.cp.einsum(
             "mn, c...m, cjn -> c...",
-            self.parameters["gcoeff"],
+            self.gcoeff,
             new_ao_val,
             masked_ao_val[:, :e, :],
             optimize=self.optimize,
         )
         new_val += gpu.cp.einsum(
             "mn, c...n, cim -> c...",
-            self.parameters["gcoeff"],
+            self.gcoeff,
             new_ao_val,
             masked_ao_val[:, e + 1 :, :],
             optimize=self.optimize,

8037858ace0c19b24c3b1e48a62e54ae5734271e
Will Wheeler <willwheelera@gmail.com>
2024-01-16 11:39:56 -0600

    change copyright to 2019-2024 The PyQMC Developers

--- tests/unit/test_sherman_morrison.py
+++ tests/unit/test_sherman_morrison.py
@@ -1,6 +1,6 @@
 # MIT License
 # 
-# Copyright (c) 2019 Lucas K Wagner
+# Copyright (c) 2019-2024 The PyQMC Developers
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal

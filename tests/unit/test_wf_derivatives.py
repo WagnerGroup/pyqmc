33dd82bf44bbe9a43dd735fe9c3f454e7b26b710
Will Wheeler <willwheelera@gmail.com>
2024-03-23 00:50:32 -0500

    add gps2 pbc test

--- tests/unit/test_wf_derivatives.py
+++ tests/unit/test_wf_derivatives.py
@@ -99,6 +99,7 @@ def test_pbc_wfs(H_pbc_sto3g_krks, epsilon=1e-5, nconf=10):
     supercell = pyq.get_supercell(mol, S=(np.ones((3, 3)) - 2 * np.eye(3)))
     epos = pyq.initial_guess(supercell, nconf)
     for wf in [
+        MultiplyWF(Slater(supercell, mf, eval_gto_precision=1e-6), generate_gps_jastrow(supercell)[0]),
         MultiplyWF(Slater(supercell, mf, eval_gto_precision=1e-6), generate_jastrow(supercell)[0]),
         MultiplyWF(
             Slater(supercell, mf, eval_gto_precision=1e-6),

1283872602c6d952495947168f8214ac40598194
Will Wheeler <willwheelera@gmail.com>
2024-03-23 00:09:39 -0500

    make gps2 work with pbcs

--- pyqmc/gps2.py
+++ pyqmc/gps2.py
@@ -19,13 +19,11 @@ class GPSJastrow:
         self._configscurrent = configs
         self.nconfig, self.nelec = configs.configs.shape[:-1]
 
-        Xsup = self.parameters["Xsupport"]
+        Xsup = self.parameters["Xsupport"][np.newaxis]
         # ds are distance from xsup to config
         d_cs = np.zeros((self.nconfig, self.n_support, self.nelec, 2, 3))
-        d_cs = (
-            configs.configs[:, np.newaxis, :, np.newaxis, :]
-            - Xsup[np.newaxis, :, np.newaxis, :, :]
-        )
+        d_cs[:, :, :, 0] = configs.dist.pairwise(Xsup[:, :, 0], configs.configs)
+        d_cs[:, :, :, 1] = configs.dist.pairwise(Xsup[:, :, 1], configs.configs)
 
         r2_cs = np.sum(d_cs**2, axis=-1)
         e_cs = np.exp(-self.parameters["f"] * r2_cs)
@@ -43,27 +41,46 @@ class GPSJastrow:
         val = self.value_from_e_cs(self.e_cs)
         return np.ones(self.nconfig), val
 
-    def _compute_e_ne(self, e, epos):
-        # shape is nsup,nconf,2,3
-        d_ne = epos[..., np.newaxis, np.newaxis, :] - self.parameters["Xsupport"]
-        # d_ne = epos[np.newaxis,:,np.newaxis,...] - self.parameters['Xsupport'][:,np.newaxis,:,:]
-        # should be ending in 4,3
+    def _compute_e_ne(self, d_ne):
+        # shape is nconf,[naux,]nsup,2,3
         r2_ne = np.sum(d_ne**2, axis=-1)
         e_ne = np.exp(-self.parameters["f"] * r2_ne)
-        return e_ne
+        return e_ne, r2_ne
+
+    def _compute_d_ne(self, e, epos):
+        # shape is nconf,[naux,]nsup,2,3
+        Xsup = self.parameters["Xsupport"][np.newaxis]
+        d_ne = np.zeros((epos.configs.shape[0], self.n_support, 2, 3))
+        d_ne[:, :, 0] = epos.dist.dist_i(Xsup[:, :, 0], epos.configs)
+        d_ne[:, :, 1] = epos.dist.dist_i(Xsup[:, :, 1], epos.configs)
+        return -d_ne
+
+    def _compute_d_ne_aux(self, e, epos):
+        # shape is nconf,[naux,]nsup,2,3
+        Xsup = self.parameters["Xsupport"][np.newaxis]
+        nconf, naux = epos.configs.shape[:2]
+        d_ne = np.zeros((nconf,self.n_support, naux2, 3))
+        d_ne[:, :, :, 0] = epos.dist.pairwise(Xsup[:, :, 0], epos.configs)
+        d_ne[:, :, :, 1] = epos.dist.pairwise(Xsup[:, :, 1], epos.configs)
+        return np.moveaxis(-d_ne, 2, 1)
 
     def updateinternals(self, e, epos, configs, mask=None, saved_values=None):
         if mask is None:
             mask = [True] * epos.configs.shape[0]
 
-        self.e_cs[mask, :, e, :] = self._compute_e_ne(e, epos.configs[mask])
+        d = self._compute_d_ne(e, epos.mask(mask))
+        self.e_cs[mask, :, e, :], _ = self._compute_e_ne(d)
         self._configscurrent.move(e, epos, mask)
 
     def testvalue(self, e, epos, mask=None):
         if mask is None:
             mask = [True] * epos.configs.shape[0]
-        is_epos_3 = len(epos.configs.shape) - 2
-        new_e_ne = self._compute_e_ne(e, epos.configs[mask])
+        is_epos_3 = len(epos.configs.shape) == 3
+        if is_epos_3:
+            d = self._compute_d_ne_aux(e, epos.mask(mask))
+        else:
+            d = self._compute_d_ne(e, epos.mask(mask))
+        new_e_ne, _ = self._compute_e_ne(d)
         tup = (slice(None),) + is_epos_3 * (None,) + (slice(None), e, slice(None))
         dif1 = new_e_ne - self.e_cs[mask][tup]
         summ = np.sum(self.e_cs[mask], axis=2)
@@ -77,17 +94,14 @@ class GPSJastrow:
         return np.exp(val), np.array([1])
 
     def gradient_value(self, e, epos):
-        new_e_ne = self._compute_e_ne(e, epos.configs)
+        d = self._compute_d_ne(e, epos)
+        new_e_ne, _ = self._compute_e_ne(d)
         dif1 = new_e_ne - self.e_cs[:, :, e, :]
         dif2 = np.sum(self.e_cs, axis=2) - self.e_cs[:, :, e, :]
 
         final = dif1[..., 0] * dif2[:, :, 1] + dif1[..., 1] * dif2[:, :, 0]
         val = np.einsum("s,c...s->c...", self.parameters["alpha"], final)
 
-        d = (
-            self.parameters["Xsupport"][np.newaxis, :, :, :]
-            - epos.configs[:, np.newaxis, np.newaxis, :]
-        )
         d = np.moveaxis(d, [0, 1, 2, 3], [1, 2, 3, 0])
         final = (
             new_e_ne[:, :, 0] * d[:, :, :, 0] * 2 * self.parameters["f"] * dif2[:, :, 1]
@@ -101,12 +115,9 @@ class GPSJastrow:
         return grad, np.exp(val), np.array([1])
 
     def gradient(self, e, epos):
-        d = (
-            self.parameters["Xsupport"][np.newaxis, :, :, :]
-            - epos.configs[:, np.newaxis, np.newaxis, :]
-        )
+        d = self._compute_d_ne(e, epos)
+        new_e_ne, _ = self._compute_e_ne(d)
         d = np.moveaxis(d, [0, 1, 2, 3], [1, 2, 3, 0])
-        new_e_ne = self._compute_e_ne(e, epos.configs)
         summ = np.sum(self.e_cs, axis=2) - self.e_cs[:, :, e, :]
         # can simplify last 2 lines by switching axis order of summ, so this fits into 1 einsum. but that will make stuff less readable, so will do
         # later
@@ -122,12 +133,9 @@ class GPSJastrow:
         return grad
 
     def gradient_laplacian(self, e, epos):
-        d = (
-            self.parameters["Xsupport"][np.newaxis, :, :, :]
-            - epos.configs[:, np.newaxis, np.newaxis, :]
-        )
+        d = self._compute_d_ne(e, epos)
+        new_e_ne, r2 = self._compute_e_ne(d)
         d = np.moveaxis(d, [0, 1, 2, 3], [1, 2, 3, 0])
-        new_e_ne = self._compute_e_ne(e, epos.configs)
         summ = np.sum(self.e_cs, axis=2) - self.e_cs[:, :, e, :]
         final = (
             new_e_ne[:, :, 0] * d[:, :, :, 0] * 2 * self.parameters["f"] * summ[:, :, 1]
@@ -140,9 +148,8 @@ class GPSJastrow:
 
         grad = np.einsum("s,dcs->dc", self.parameters["alpha"], final)
         d = np.moveaxis(d, [0, 1, 2, 3], [3, 0, 1, 2])
-        r = np.sum(d**2, axis=-1)
 
-        term1 = 4 * self.parameters["f"] ** 2 * r - 6 * self.parameters["f"]
+        term1 = 4 * self.parameters["f"] ** 2 * r2 - 6 * self.parameters["f"]
         final = (
             term1[:, :, 0] * new_e_ne[:, :, 0] * summ[:, :, 1]
             + term1[:, :, 1] * new_e_ne[:, :, 1] * summ[:, :, 0]
@@ -152,18 +159,11 @@ class GPSJastrow:
         return grad, lap
 
     def laplacian(self, e, epos):
-
-        d = (
-            self.parameters["Xsupport"][np.newaxis, :, :, :]
-            - epos.configs[:, np.newaxis, np.newaxis, :]
-        )
-
-        r = np.sum(d**2, axis=-1)
-
-        new_e_ne = self._compute_e_ne(e, epos.configs)
+        d = self._compute_d_ne(e, epos)
+        new_e_ne, r2 = self._compute_e_ne(d)
         summ = np.sum(self.e_cs, axis=2) - self.e_cs[:, :, e, :]
 
-        term1 = 4 * self.parameters["f"] ** 2 * r - 6 * self.parameters["f"]
+        term1 = 4 * self.parameters["f"] ** 2 * r2 - 6 * self.parameters["f"]
         final = (
             term1[:, :, 0] * new_e_ne[:, :, 0] * summ[:, :, 1]
             + term1[:, :, 1] * new_e_ne[:, :, 1] * summ[:, :, 0]
@@ -176,29 +176,30 @@ class GPSJastrow:
 
     # configs shape nconfig,nelec,3
     def pgradient(self):
-        configs = self._configscurrent.configs
+        configs = self._configscurrent
         A = (self.e_cs - np.sum(self.e_cs, axis=2, keepdims=True))[:, :, :, ::-1]
         alphader = -np.einsum("csi,csi->cs", A[:, :, :, 0], A[:, :, :, 1])
 
-        d = (
-            self.parameters["Xsupport"][np.newaxis, :, np.newaxis, :, :]
-            - configs[:, np.newaxis, :, np.newaxis, :]
-        )
-        r = np.sum(d**2, axis=-1)
+        Xsup = self.parameters["Xsupport"][np.newaxis]
+        d_cs = np.zeros((self.nconfig, self.n_support, self.nelec, 2, 3))
+        d_cs[:, :, :, 0] = configs.dist.pairwise(Xsup[:, :, 0], configs.configs)
+        d_cs[:, :, :, 1] = configs.dist.pairwise(Xsup[:, :, 1], configs.configs)
+
+        r2 = np.sum(d_cs**2, axis=-1)
 
         fder = np.einsum(
             "s,csit,csit,csit->c",
             self.parameters["alpha"],
             self.e_cs,
             A,
-            r,
+            r2,
             optimize="greedy",
         )
 
         Xder = np.einsum(
             "s,csitd,csit,csit->cstd",
             2 * self.parameters["f"] * self.parameters["alpha"],
-            d,
+            d_cs,
             self.e_cs,
             A,
             optimize="greedy",

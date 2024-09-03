639cad831283f4f815baa9399b5c9854343f7873
Will Wheeler <willwheelera@gmail.com>
2024-09-03 12:13:11 -0700

    remove timing from eval_gto example

--- examples/eval_gto_python.py
+++ examples/eval_gto_python.py
@@ -1,6 +1,5 @@
 import pyqmc.api as pyq
 import pyscf
-import time
 import scipy.stats
 import numpy as np
 
@@ -13,15 +12,10 @@ configs = pyq.initial_guess(mol, 10)
 # Use pyscf's eval_gto() function
 wf, to_opt = pyq.generate_wf(mol, mf, slater_kws=dict(evaluate_orbitals_with="pyscf"))
 print(wf.wf_factors[0].orbitals.eval_gto)
-t0 = time.perf_counter()
 pyq.vmc(wf, configs, accumulators={"energy": enacc}, nblocks=1)
-print("time pyscf", time.perf_counter() - t0)
 
 # Use pyqmc's numba implementation. Sometimes faster
 wf, _ = pyq.generate_wf(mol, mf, slater_kws=dict(evaluate_orbitals_with="numba"))
 print(wf.wf_factors[0].orbitals.eval_gto)
 # The first time you call numba functions, they must compile, so this might take a bit longer than eval_gto() for a short run
-for i in range(2):
-    t0 = time.perf_counter()
-    pyq.vmc(wf, configs, accumulators={"energy": enacc}, nblocks=1)
-    print("time numba", i, time.perf_counter() - t0)
+pyq.vmc(wf, configs, accumulators={"energy": enacc}, nblocks=1)

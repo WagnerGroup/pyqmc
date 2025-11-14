310e63cb9cf0ca65324c88a9830fe9166887b5f5
Lucas Wagner <lucas.wagner@gmail.com>
2025-11-14 09:38:04 -0600

    Remove laplacian() from wf objects. (#493)

--- pyqmc/wf/testwf.py
+++ pyqmc/wf/testwf.py
@@ -217,55 +217,10 @@ def test_wf_pgradient(wf, configs, delta=1e-5):
     return max(error.values())  # Return maximum coefficient error
 
 
-def test_wf_laplacian(wf, configs, delta=1e-5):
-    """Tests wf.laplacian(e,epos) against numerical derivatives of wf.gradient(e,epos)
-
-    :parameter wf: a wavefunction object with functions wf.recompute(configs),
-             wf.gradient(e,configs) and wf.laplacian(e,configs)
-    :parameter configs: positions to set the wf object
-    :type configs: (nconf, nelec, 3) array
-    :parameter float delta: the finite difference step; 1e-5 to 1e-6 seem to be the best compromise between accuracy and machine precision
-
-    For gradient and laplacian:
-        e is the electron index
-        epos is nconf x 3 positions of electron e
-
-    wf.gradient(e,epos) should return grad ln Psi(epos), while keeping all the other electrons at current position. epos may be different from the current position of electron e
-    wf.laplacian(e,epos) should behave the same as gradient, except lap(Psi(epos))/Psi(epos)
-
-    :returns: max abs errors
-    :rtype: dictionary
-    """
-    nconf, nelec = configs.configs.shape[0:2]
-    wf.recompute(configs)
-    maxerror = 0
-    lap = np.zeros(configs.configs.shape[:2], dtype=wf.dtype)
-    numeric = np.zeros(configs.configs.shape[:2], dtype=wf.dtype)
-
-    for e in range(nelec):
-        lap[:, e] = wf.laplacian(e, configs.electron(e))
-
-        for d in range(3):
-            epos = configs.make_irreducible(
-                e, configs.configs[:, e, :] + delta * np.eye(3)[d]
-            )
-            plusval, _ = wf.testvalue(e, epos)
-            plusgrad = wf.gradient(e, epos)[d] * plusval
-            epos = configs.make_irreducible(
-                e, configs.configs[:, e, :] - delta * np.eye(3)[d]
-            )
-            minuval, _ = wf.testvalue(e, epos)
-            minugrad = wf.gradient(e, epos)[d] * minuval
-            numeric[:, e] += (plusgrad - minugrad) / (2 * delta)
-
-    maxerror = np.amax(np.abs(lap - numeric))
-    return maxerror
-
 
 def test_wf_gradient_laplacian(wf, configs):
     nconf, nelec = configs.configs.shape[0:2]
     wf.recompute(configs)
-    lap = np.zeros(configs.configs.shape[:2], dtype=wf.dtype)
     grad = np.zeros(configs.configs.shape, dtype=wf.dtype).transpose((1, 2, 0))
     andlap = np.zeros(configs.configs.shape[:2], dtype=wf.dtype)
     andgrad = np.zeros(configs.configs.shape, dtype=wf.dtype).transpose((1, 2, 0))
@@ -274,7 +229,6 @@ def test_wf_gradient_laplacian(wf, configs):
     ttog = 0
     for e in range(nelec):
         ts0 = time.perf_counter()
-        lap[:, e] = wf.laplacian(e, configs.electron(e))
         grad[e] = wf.gradient(e, configs.electron(e))
         ts1 = time.perf_counter()
         tt0 = time.perf_counter()
@@ -283,15 +237,12 @@ def test_wf_gradient_laplacian(wf, configs):
         tsep += ts1 - ts0
         ttog += tt1 - tt0
     rel_grad = np.abs((andgrad - grad) / grad)
-    rel_lap = np.abs((andlap - lap) / lap)
     rmax_grad = np.max(rel_grad)
-    rmax_lap = np.max(rel_lap)
 
     print("time evaluated separately", tsep)
     print("time evaluated together", ttog)
 
-    return {"grad": rmax_grad, "lap": rmax_lap}
-
+    return {"grad": rmax_grad}
 
 def compare_nested_saved_vals(saved1, saved2):
     if saved1 is None:
